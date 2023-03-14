4f4a62532bcc582ca6d96503c43b2dbc300fc708
edg-l <edg-l@users.noreply.github.com>
2023-03-14 16:03:43 +0000

    deploy: 7986935f7f1b3e0aedb5ab1a2d666507a8310bc7

--- implementors/core/clone/trait.Clone.js
+++ implementors/core/clone/trait.Clone.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"mlir":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.1/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"enum\" href=\"mlir/mlir_type/enum.DataType.html\" title=\"enum mlir::mlir_type::DataType\">DataType</a>"]]
+"sierra2mlir":[["impl&lt;'ctx&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"enum\" href=\"sierra2mlir/compiler/enum.SierraType.html\" title=\"enum sierra2mlir::compiler::SierraType\">SierraType</a>&lt;'ctx&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- sierra2mlir/compiler/sidebar-items.js
+++ sierra2mlir/compiler/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":[["Compiler",""],["Storage","Types, functions, etc storage. This aproach works better with lifetimes."]]};
+window.SIDEBAR_ITEMS = {"enum":[["SierraType",""]],"struct":[["Compiler",""],["Storage","Types, functions, etc storage. This aproach works better with lifetimes."]]};
