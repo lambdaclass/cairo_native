13859a63c1decf38f590b5f3a5bfcdf5d2784a12
Esteve Soler Arderiu <esteve.soler@lambdaclass.com>
2023-03-29 19:02:42 +0200

    Implement felt division.

--- sierra2mlir/src/libfuncs/mod.rs
+++ sierra2mlir/src/libfuncs/mod.rs
@@ -518,7 +518,7 @@ impl<'ctx> Compiler<'ctx> {
                 let rhs_zext = self.op_zext(&entry_block, rhs.into(), self.double_felt_type());
                 self.op_mul(&entry_block, lhs_zext.result(0)?.into(), rhs_zext.result(0)?.into())
             }
-            BinaryOp::Div => todo!(),
+            BinaryOp::Div => self.op_felt_div(&region, &entry_block, lhs.into(), rhs.into())?,
         };
         let res_result = res.result(0)?;
 
