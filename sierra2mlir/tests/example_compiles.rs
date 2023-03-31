2de6ad66b931164bfd532cf5cbaa633e159c3701
Esteve Soler Arderiu <esteve.soler@lambdaclass.com>
2023-03-31 14:30:41 +0200

    Fix tests.

--- sierra2mlir/tests/example_compiles.rs
+++ sierra2mlir/tests/example_compiles.rs
@@ -3,13 +3,15 @@ macro_rules! impl_tests {
         $(
             #[test]
             fn $name() {
+                use cairo_lang_sierra::ProgramParser;
                 use std::fs::read_to_string;
 
                 let program_path = concat!("../examples/", stringify!($name), ".sierra");
                 let sierra_source =
                     read_to_string(program_path).expect("Could not read Sierra source code");
 
-                sierra2mlir::compile(&sierra_source, false, false, false, 1).expect("Error compiling sierra program");
+                let program = ProgramParser::new().parse(&sierra_source).unwrap();
+                sierra2mlir::compile(&program, false, false, false, 1).expect("Error compiling sierra program");
             }
         )*
     };
