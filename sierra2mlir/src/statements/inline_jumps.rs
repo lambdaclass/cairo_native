5793f99b1727056ef7aba1de038f582891dc775f
Edgar Luque <git@edgarluque.com>
2023-04-14 17:15:40 +0200

    implement uint lq eq le

--- sierra2mlir/src/statements/inline_jumps.rs
+++ sierra2mlir/src/statements/inline_jumps.rs
@@ -106,7 +106,61 @@ impl<'ctx> Compiler<'ctx> {
         Ok(())
     }
 
-    #[allow(clippy::too_many_arguments)]
+    // eq, le, lt
+    pub fn inline_int_cmpop(
+        &'ctx self,
+        id: &str,
+        invocation: &Invocation,
+        block: &Block<'ctx>,
+        variables: &HashMap<u64, Variable>,
+        blocks: &BTreeMap<usize, BlockInfo<'ctx>>,
+        statement_idx: usize,
+        storage: &Storage,
+        cmpop: CmpOp,
+    ) -> Result<()> {
+        let libfunc = storage.libfuncs.get(id).unwrap();
+        let pos_arg_1 = &libfunc.get_args()[0];
+        let pos_arg_2 = &libfunc.get_args()[1];
+
+        let arg1 = variables
+            .get(&invocation.args[pos_arg_1.loc].id)
+            .expect("Variable should be registered before use")
+            .get_value();
+
+        let arg2 = variables
+            .get(&invocation.args[pos_arg_2.loc].id)
+            .expect("Variable should be registered before use")
+            .get_value();
+
+        let eq_op = self.op_cmp(block, cmpop, arg1, arg2);
+        let eq = eq_op.result(0)?;
+
+        let target_blocks = invocation
+            .branches
+            .iter()
+            .map(|branch| match branch.target {
+                GenBranchTarget::Fallthrough => statement_idx + 1,
+                GenBranchTarget::Statement(idx) => idx.0,
+            })
+            .map(|idx| {
+                let target_block_info = blocks.get(&idx).unwrap();
+                let operand_values = target_block_info
+                    .variables_at_start
+                    .keys()
+                    .map(|id| variables.get(id).unwrap().get_value())
+                    .collect_vec();
+                (&target_block_info.block, operand_values)
+            })
+            .collect_vec();
+
+        let (true_block, true_vars) = &target_blocks[0];
+        let (false_block, false_vars) = &target_blocks[1];
+
+        self.op_cond_br(block, eq.into(), true_block, false_block, true_vars, false_vars);
+
+        Ok(())
+    }
+
     pub fn inline_enum_match(
         &self,
         id: &str,
@@ -252,7 +306,7 @@ impl<'ctx> Compiler<'ctx> {
             let length: Value = length_op.result(0)?.into();
 
             // check if index is out of bounds
-            let cmp_op = self.op_cmp(block, CmpOp::UnsignedLess, index_var.get_value(), length);
+            let cmp_op = self.op_cmp(block, CmpOp::UnsignedLessThan, index_var.get_value(), length);
             let cmp = cmp_op.result(0)?.into();
 
             let block_get_idx = region.append_block(Block::new(&[]));
@@ -316,7 +370,6 @@ impl<'ctx> Compiler<'ctx> {
         }
     }
 
-    #[allow(clippy::too_many_arguments)]
     pub fn inline_array_pop_front(
         &self,
         id: &str,
@@ -367,7 +420,7 @@ impl<'ctx> Compiler<'ctx> {
             let length: Value = length_op.result(0)?.into();
 
             // check if there is something to pop
-            let cmp_op = self.op_cmp(block, CmpOp::UnsignedGreaterEqual, length, const_1);
+            let cmp_op = self.op_cmp(block, CmpOp::UnsignedGreaterThanEqual, length, const_1);
             let cmp = cmp_op.result(0)?.into();
 
             let block_pop_idx = region.append_block(Block::new(&[]));
