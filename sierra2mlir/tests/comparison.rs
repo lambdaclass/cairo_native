201ff23612e5c0a94d556459c29138e39d54e945
Esteve Soler Arderiu <esteve.soler@fratilia.com>
2023-04-26 18:23:20 +0200

    Add comparison test.

--- sierra2mlir/tests/comparison.rs
+++ sierra2mlir/tests/comparison.rs
@@ -48,6 +48,7 @@ use test_case::test_case;
 #[test_case("fib_local")]
 #[test_case("nullable/test_nullable")]
 #[test_case("pedersen")]
+#[test_case("poseidon")]
 #[test_case("returns/enums")]
 #[test_case("returns/simple")]
 #[test_case("returns/tuple")]
