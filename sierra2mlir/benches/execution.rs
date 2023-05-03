8a10965abc88aeec184a3ffbd1e9f0784c34ac73
Edgar Luque <git@edgarluque.com>
2023-05-03 17:40:42 +0200

    fix segfault on bench and without main print

--- sierra2mlir/benches/execution.rs
+++ sierra2mlir/benches/execution.rs
@@ -1,29 +1,35 @@
 use cairo_lang_sierra::ProgramParser;
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use std::{env, fs};
 
-pub fn criterion_benchmark(c: &mut Criterion) {
-    //let program = ProgramParser::new().parse(include_str!("programs/fib.sierra")).unwrap();
-    //let engine = sierra2mlir::execute(&program, false, 1, Some(800_000_000)).unwrap();
+// Source: the generated MLIR from fib.sierra
+#[derive(Debug, Default)]
+#[repr(C, packed)]
+struct ReturnValue {
+    pub tag: i16,
+    pub data: [i8; 16],
+}
 
-    /* TODO: re-enable when segfault is fixed, it happens upon calling the invoke_packed,
-        either due to missining pointers for return values or something else.
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let program = ProgramParser::new().parse(include_str!("programs/fib.sierra")).unwrap();
+    let engine = sierra2mlir::execute(&program, false, 1, Some(800_000_000)).unwrap();
+
+    let mut return_value = ReturnValue::default();
+    let mut return_ptr = std::ptr::addr_of_mut!(return_value);
+    let return_ptr_ptr = std::ptr::addr_of_mut!(return_ptr);
     unsafe {
-        engine
-            .invoke_packed("fib::fib::main", &mut [])
-            .unwrap();
+        engine.invoke_packed("fib::fib::main", &mut [return_ptr_ptr as *mut _ as *mut ()]).unwrap();
     };
 
     c.bench_with_input(BenchmarkId::new("MLIR", 1), &(engine), |b, engine| {
         b.iter(|| {
             unsafe {
-                let mut return_value = ReturnValue::default();
-                let return_ptr = std::ptr::addr_of_mut!(return_value);
-                engine.invoke_packed("fib::fib::main", &mut [return_ptr as *mut _ as *mut ()]).ok();
+                engine
+                    .invoke_packed("fib::fib::main", &mut [return_ptr_ptr as *mut _ as *mut ()])
+                    .ok();
             };
         });
     });
-    */
 
     // Requires sierra files to be generated previously.
 
