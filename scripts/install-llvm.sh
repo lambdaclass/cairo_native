8de97c0118f59127925060e83f6a31365c36df21
Esteve Soler Arderiu <esteve.soler@lambdaclass.com>
2023-05-26 16:27:31 +0200

    Update LLVM installation script.

--- scripts/install-llvm.sh
+++ scripts/install-llvm.sh
@@ -7,9 +7,9 @@ set -e
 mkdir -p llvm/dist
 mkdir -p llvm/source
 
-wget -P llvm/source "https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.0/llvm-project-16.0.0.src.tar.xz"
-tar -xf "llvm/source/llvm-project-16.0.0.src.tar.xz" -C llvm/source/
-cd "llvm/source/llvm-project-16.0.0.src/"
+wget -P llvm/source "https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.4/llvm-project-16.0.4.src.tar.xz"
+tar -xf "llvm/source/llvm-project-16.0.4.src.tar.xz" -C llvm/source/
+cd "llvm/source/llvm-project-16.0.4.src/"
 
 mkdir -p build/
 cd build/
@@ -17,13 +17,13 @@ cd build/
 cmake ../llvm \
     -DCMAKE_BUILD_TYPE=RelWithDebInfo \
     -DCMAKE_INSTALL_PREFIX="../../../dist/" \
-    -DLLVM_BUILD_EXAMPLES=ON \
+    -DLLVM_BUILD_EXAMPLES=OFF \
     -DLLVM_ENABLE_ASSERTIONS=ON \
-    -DLLVM_ENABLE_PROJECTS=mlir \
+    -DLLVM_ENABLE_PROJECTS="mlir;clang;lldb" \
     -DLLVM_INSTALL_UTILS=ON \
-    -DLLVM_TARGETS_TO_BUILD="X86;AArch64;NVPTX;AMDGPU"
+    -DLLVM_TARGETS_TO_BUILD="X86;AArch64"
 
-cmake --build .
+cmake --build . -j$(nproc)
 cmake --install .
 
 echo "Please set MLIR_SYS_160_PREFIX to $(realpath ../../../dist/)"
