eb537bf09b5a2e26a3fdc24102157b40315735e4
Esteve Soler Arderiu <esteve.soler@fratilia.com>
2023-05-29 16:16:33 +0200

    Make dep check script work with crates.rs dependencies.

--- .github/scripts/check-git-deps.sh
+++ .github/scripts/check-git-deps.sh
@@ -12,7 +12,7 @@ latest_release=$(
   )
 
 current_release=$(
-    rg 'cairo-lang-sierra\s*=\s*\{\s*git\s*=\s*"https://github.com/starkware-libs/cairo",\s*tag = "(.*?)"\s*\}' sierra2mlir/Cargo.toml -r '$1'
+    rg 'cairo-lang-sierra\s*=\s*(?:\{\s*git\s*=\s*"https://github.com/starkware-libs/cairo",\s*tag = "(.*?)"\s*\}|"(.*?)")' sierra2mlir/Cargo.toml -r '$1$2'
 )
 
 current_release_line=$(echo $current_release | cut -d':' -f1)
