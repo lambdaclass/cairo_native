17c52d8ccd17f251067a65bb0b726671f9f9a159
Esteve Soler Arderiu <esteve.soler@lambdaclass.com>
2023-06-21 00:05:39 +0200

    Implement `enum_init` and fix stuff.

--- src/types/enum.rs
+++ src/types/enum.rs
@@ -13,17 +13,40 @@ use melior::{
 
 pub fn build<'ctx, TType, TLibfunc>(
     context: &'ctx Context,
-    _module: &Module<'ctx>,
-    _registry: &ProgramRegistry<TType, TLibfunc>,
-    _metadata: &mut MetadataStorage,
-    _info: &EnumConcreteType,
+    module: &Module<'ctx>,
+    registry: &ProgramRegistry<TType, TLibfunc>,
+    metadata: &mut MetadataStorage,
+    info: &EnumConcreteType,
 ) -> Result<Type<'ctx>, std::convert::Infallible>
 where
     TType: GenericType,
     TLibfunc: GenericLibfunc,
     <TType as GenericType>::Concrete: TypeBuilder,
 {
-    Ok(llvm::r#type::opaque_pointer(context))
+    let (tag_ty, variant_tys, _) =
+        get_type_for_variants(context, module, registry, metadata, &info.variants).unwrap();
+
+    let tag_size = crate::ffi::get_size(module, &tag_ty);
+    let payload_size = variant_tys
+        .iter()
+        .map(|(variant_ty, _)| crate::ffi::get_size(module, variant_ty))
+        .max()
+        .unwrap_or_default();
+
+    Ok(llvm::r#type::pointer(
+        llvm::r#type::r#struct(
+            context,
+            &[
+                tag_ty,
+                llvm::r#type::array(
+                    IntegerType::new(context, 8).into(),
+                    (payload_size - tag_size).try_into().unwrap(),
+                ),
+            ],
+            false,
+        ),
+        0,
+    ))
 }
 
 pub fn get_type_for_variants<'ctx, TType, TLibfunc>(
@@ -32,7 +55,7 @@ pub fn get_type_for_variants<'ctx, TType, TLibfunc>(
     registry: &ProgramRegistry<TType, TLibfunc>,
     metadata: &mut MetadataStorage,
     variants: &[ConcreteTypeId],
-) -> Result<(Type<'ctx>, Vec<(Type<'ctx>, Type<'ctx>)>), std::convert::Infallible>
+) -> Result<(Type<'ctx>, Vec<(Type<'ctx>, Type<'ctx>)>, usize), std::convert::Infallible>
 where
     TType: GenericType,
     TLibfunc: GenericLibfunc,
@@ -41,6 +64,7 @@ where
     let tag_ty: Type =
         IntegerType::new(context, variants.len().next_power_of_two().trailing_zeros()).into();
 
+    let mut align = 0;
     let mut output = Vec::with_capacity(variants.len());
     for variant in variants {
         let payload_ty = registry
@@ -49,11 +73,11 @@ where
             .build(context, module, registry, metadata)
             .unwrap();
 
-        output.push((
-            llvm::r#type::r#struct(context, &[tag_ty, payload_ty], false),
-            payload_ty,
-        ));
+        let variant_ty = llvm::r#type::r#struct(context, &[tag_ty, payload_ty], false);
+        align = align.max(crate::ffi::get_abi_alignment(module, &variant_ty));
+
+        output.push((variant_ty, payload_ty));
     }
 
-    Ok((tag_ty, output))
+    Ok((tag_ty, output, align))
 }
