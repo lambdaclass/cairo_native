d959736d510583ec2b907cc189016cb46bf1ef25
Esteve Soler Arderiu <esteve.soler@lambdaclass.com>
2023-06-21 00:23:34 +0200

    Add `get_builtin_costs` placeholder.

--- src/libfuncs/gas.rs
+++ src/libfuncs/gas.rs
@@ -1,1 +1,69 @@
+use super::{LibfuncBuilder, LibfuncHelper};
+use crate::{metadata::MetadataStorage, types::TypeBuilder};
+use cairo_lang_sierra::{
+    extensions::{
+        gas::GasConcreteLibfunc, lib_func::SignatureOnlyConcreteLibfunc, ConcreteLibfunc,
+        GenericLibfunc, GenericType,
+    },
+    program_registry::ProgramRegistry,
+};
+use melior::{
+    dialect::llvm,
+    ir::{Block, Location},
+    Context,
+};
 
+pub fn build<'ctx, 'this, TType, TLibfunc>(
+    context: &'ctx Context,
+    registry: &ProgramRegistry<TType, TLibfunc>,
+    entry: &'this Block<'ctx>,
+    location: Location<'ctx>,
+    helper: &LibfuncHelper<'ctx, 'this>,
+    metadata: &mut MetadataStorage,
+    selector: &GasConcreteLibfunc,
+) -> Result<(), std::convert::Infallible>
+where
+    TType: GenericType,
+    TLibfunc: GenericLibfunc,
+    <TType as GenericType>::Concrete: TypeBuilder,
+    <TLibfunc as GenericLibfunc>::Concrete: LibfuncBuilder,
+{
+    match selector {
+        GasConcreteLibfunc::WithdrawGas(_) => todo!(),
+        GasConcreteLibfunc::RedepositGas(_) => todo!(),
+        GasConcreteLibfunc::GetAvailableGas(_) => todo!(),
+        GasConcreteLibfunc::BuiltinWithdrawGas(_) => todo!(),
+        GasConcreteLibfunc::GetBuiltinCosts(info) => {
+            build_get_builtin_costs(context, registry, entry, location, helper, metadata, info)
+        }
+    }
+}
+
+pub fn build_get_builtin_costs<'ctx, 'this, TType, TLibfunc>(
+    context: &'ctx Context,
+    registry: &ProgramRegistry<TType, TLibfunc>,
+    entry: &'this Block<'ctx>,
+    location: Location<'ctx>,
+    helper: &LibfuncHelper<'ctx, 'this>,
+    metadata: &mut MetadataStorage,
+    info: &SignatureOnlyConcreteLibfunc,
+) -> Result<(), std::convert::Infallible>
+where
+    TType: GenericType,
+    TLibfunc: GenericLibfunc,
+    <TType as GenericType>::Concrete: TypeBuilder,
+    <TLibfunc as GenericLibfunc>::Concrete: LibfuncBuilder,
+{
+    let builtin_costs_ty = registry
+        .get_type(&info.branch_signatures()[0].vars[0].ty)
+        .unwrap()
+        .build(context, helper, registry, metadata)
+        .unwrap();
+
+    // TODO: Implement libfunc.
+    let op0 = entry.append_operation(llvm::undef(builtin_costs_ty, location));
+
+    entry.append_operation(helper.br(0, &[op0.result(0).unwrap().into()], location));
+
+    Ok(())
+}
