3dd0f122164fafe71d98196f070a0c6dc5bac122
Esteve Soler Arderiu <esteve.soler@lambdaclass.com>
2023-06-21 01:29:10 +0200

    Implement `array_new` libfunc.

--- src/libfuncs/array.rs
+++ src/libfuncs/array.rs
@@ -1,1 +1,96 @@
+use super::{LibfuncBuilder, LibfuncHelper};
+use crate::{metadata::MetadataStorage, types::TypeBuilder};
+use cairo_lang_sierra::{
+    extensions::{
+        array::ArrayConcreteLibfunc, lib_func::SignatureOnlyConcreteLibfunc, ConcreteLibfunc,
+        GenericLibfunc, GenericType,
+    },
+    program_registry::ProgramRegistry,
+};
+use melior::{
+    dialect::{arith, llvm},
+    ir::{
+        attribute::{DenseI64ArrayAttribute, IntegerAttribute},
+        r#type::IntegerType,
+        Block, Location,
+    },
+    Context,
+};
 
+pub fn build<'ctx, 'this, TType, TLibfunc>(
+    context: &'ctx Context,
+    registry: &ProgramRegistry<TType, TLibfunc>,
+    entry: &'this Block<'ctx>,
+    location: Location<'ctx>,
+    helper: &LibfuncHelper<'ctx, 'this>,
+    metadata: &mut MetadataStorage,
+    selector: &ArrayConcreteLibfunc,
+) -> Result<(), std::convert::Infallible>
+where
+    TType: GenericType,
+    TLibfunc: GenericLibfunc,
+    <TType as GenericType>::Concrete: TypeBuilder,
+    <TLibfunc as GenericLibfunc>::Concrete: LibfuncBuilder,
+{
+    match selector {
+        ArrayConcreteLibfunc::New(info) => {
+            build_new(context, registry, entry, location, helper, metadata, info)
+        }
+        ArrayConcreteLibfunc::Append(_) => todo!(),
+        ArrayConcreteLibfunc::PopFront(_) => todo!(),
+        ArrayConcreteLibfunc::PopFrontConsume(_) => todo!(),
+        ArrayConcreteLibfunc::Get(_) => todo!(),
+        ArrayConcreteLibfunc::Slice(_) => todo!(),
+        ArrayConcreteLibfunc::Len(_) => todo!(),
+        ArrayConcreteLibfunc::SnapshotPopFront(_) => todo!(),
+        ArrayConcreteLibfunc::SnapshotPopBack(_) => todo!(),
+    }
+}
+
+pub fn build_new<'ctx, 'this, TType, TLibfunc>(
+    context: &'ctx Context,
+    registry: &ProgramRegistry<TType, TLibfunc>,
+    entry: &'this Block<'ctx>,
+    location: Location<'ctx>,
+    helper: &LibfuncHelper<'ctx, 'this>,
+    metadata: &mut MetadataStorage,
+    info: &SignatureOnlyConcreteLibfunc,
+) -> Result<(), std::convert::Infallible>
+where
+    TType: GenericType,
+    TLibfunc: GenericLibfunc,
+    <TType as GenericType>::Concrete: TypeBuilder,
+    <TLibfunc as GenericLibfunc>::Concrete: LibfuncBuilder,
+{
+    let array_ty = registry
+        .get_type(&info.branch_signatures()[0].vars[0].ty)
+        .unwrap()
+        .build(context, helper, registry, metadata)
+        .unwrap();
+
+    let op0 = entry.append_operation(arith::constant(
+        context,
+        IntegerAttribute::new(0, IntegerType::new(context, 32).into()).into(),
+        location,
+    ));
+
+    let op1 = entry.append_operation(llvm::undef(array_ty, location));
+    let op2 = entry.append_operation(llvm::insert_value(
+        context,
+        op1.result(0).unwrap().into(),
+        DenseI64ArrayAttribute::new(context, &[1]),
+        op0.result(0).unwrap().into(),
+        location,
+    ));
+    let op3 = entry.append_operation(llvm::insert_value(
+        context,
+        op2.result(0).unwrap().into(),
+        DenseI64ArrayAttribute::new(context, &[1]),
+        op0.result(0).unwrap().into(),
+        location,
+    ));
+
+    entry.append_operation(helper.br(0, &[op3.result(0).unwrap().into()], location));
+
+    Ok(())
+}
