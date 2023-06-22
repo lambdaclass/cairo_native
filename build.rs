fbe11c6e3abc389c31c967eefae0a41bd9bba5bf
Edgar Luque <git@edgarluque.com>
2023-06-22 13:47:15 +0200

    fix compilation

--- build.rs
+++ build.rs
@@ -1,8 +1,10 @@
+use std::env::var;
+
 fn main() {
     cc::Build::new()
         .cpp(true)
         .flag_if_supported("-std=c++17")
-        .flag_if_supported("-I/usr/lib/llvm-16/include")
+        .flag_if_supported(&format!("-I{}/include", var("MLIR_SYS_160_PREFIX").unwrap()))
         .flag_if_supported("-Wno-unused-parameter")
         .file("src/ffi.cpp")
         .compile("ffi");
