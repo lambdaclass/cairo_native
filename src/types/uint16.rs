29f8da64ddc34954c96b615737d6ab021f7a5f20
Edgar Luque <git@edgarluque.com>
2023-06-22 14:19:14 +0200

    build struct type using melior, misc uint

--- src/types/uint16.rs
+++ src/types/uint16.rs
@@ -1,1 +1,25 @@
+use super::TypeBuilder;
+use crate::metadata::MetadataStorage;
+use cairo_lang_sierra::{
+    extensions::{types::InfoOnlyConcreteType, GenericLibfunc, GenericType},
+    program_registry::ProgramRegistry,
+};
+use melior::{
+    ir::{r#type::IntegerType, Module, Type},
+    Context,
+};
 
+pub fn build<'ctx, TType, TLibfunc>(
+    context: &'ctx Context,
+    _module: &Module<'ctx>,
+    _registry: &ProgramRegistry<TType, TLibfunc>,
+    _metadata: &mut MetadataStorage,
+    _info: &InfoOnlyConcreteType,
+) -> Result<Type<'ctx>, std::convert::Infallible>
+where
+    TType: GenericType,
+    TLibfunc: GenericLibfunc,
+    <TType as GenericType>::Concrete: TypeBuilder,
+{
+    Ok(IntegerType::new(context, 16).into())
+}
