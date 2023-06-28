0a4188383c368b86622971570f4813bd5c55c770
Edgar <git@edgarluque.com>
2023-06-29 00:46:36 +0200

    implement snapshot type and more libfuncs (#187)
    * implement snapshot type and libfunc, add cli options to optimize and lower to llvm
    
    * remove options
    
    * more libfuncs
    
    * remove  dbg
    
    * serialize uints
    
    * fix array
    
    * array serialize
    
    * cleanup
    
    * array example, which is broken

--- src/libfuncs/snapshot_take.rs
+++ src/libfuncs/snapshot_take.rs
@@ -1,3 +1,45 @@
 //! # Snapshot taking libfuncs
 //!
 //! TODO
+
+use super::{LibfuncBuilder, LibfuncHelper};
+use crate::{metadata::MetadataStorage, types::TypeBuilder};
+use cairo_lang_sierra::{
+    extensions::{lib_func::SignatureOnlyConcreteLibfunc, GenericLibfunc, GenericType},
+    program_registry::ProgramRegistry,
+};
+use melior::{
+    ir::{Block, Location},
+    Context,
+};
+
+/// Generate MLIR operations for the `snapshot_take` libfunc.
+pub fn build<'ctx, 'this, TType, TLibfunc>(
+    _context: &'ctx Context,
+    _registry: &ProgramRegistry<TType, TLibfunc>,
+    entry: &'this Block<'ctx>,
+    location: Location<'ctx>,
+    helper: &LibfuncHelper<'ctx, 'this>,
+    _metadata: &mut MetadataStorage,
+    _info: &SignatureOnlyConcreteLibfunc,
+) -> Result<(), std::convert::Infallible>
+where
+    TType: GenericType,
+    TLibfunc: GenericLibfunc,
+    <TType as GenericType>::Concrete: TypeBuilder,
+    <TLibfunc as GenericLibfunc>::Concrete: LibfuncBuilder,
+{
+    // TODO: Should this act like dup like it does now? or are there special requirements. So far it seems to work.
+    // TODO: Handle non-trivially-copyable types (ex. arrays) and maybe update docs.
+
+    entry.append_operation(helper.br(
+        0,
+        &[
+            entry.argument(0).unwrap().into(),
+            entry.argument(0).unwrap().into(),
+        ],
+        location,
+    ));
+
+    Ok(())
+}
--- src/values/uint16.rs
+++ src/values/uint16.rs
@@ -1,1 +1,58 @@
+use super::ValueBuilder;
+use cairo_lang_sierra::{
+    extensions::{types::InfoOnlyConcreteType, GenericLibfunc, GenericType},
+    ids::ConcreteTypeId,
+    program_registry::ProgramRegistry,
+};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use serde_json::Number;
+use std::{fmt, ptr::NonNull, str::FromStr};
 
+pub unsafe fn deserialize<'de, TType, TLibfunc, D>(
+    deserializer: D,
+    _registry: &ProgramRegistry<TType, TLibfunc>,
+    ptr: NonNull<()>,
+    _info: &InfoOnlyConcreteType,
+) -> Result<(), D::Error>
+where
+    TType: GenericType,
+    TLibfunc: GenericLibfunc,
+    <TType as GenericType>::Concrete: ValueBuilder<TType, TLibfunc>,
+    D: Deserializer<'de>,
+{
+    let value = <Number as Deserialize>::deserialize(deserializer)?;
+    let value: u16 = value.to_string().parse().unwrap();
+    std::ptr::write(ptr.cast::<u16>().as_mut(), value);
+    Ok(())
+}
+
+pub unsafe fn serialize<TType, TLibfunc, S>(
+    serializer: S,
+    _registry: &ProgramRegistry<TType, TLibfunc>,
+    ptr: NonNull<()>,
+    _info: &InfoOnlyConcreteType,
+) -> Result<S::Ok, S::Error>
+where
+    TType: GenericType,
+    TLibfunc: GenericLibfunc,
+    <TType as GenericType>::Concrete: ValueBuilder<TType, TLibfunc>,
+    S: Serializer,
+{
+    let value = ptr.cast::<u16>().as_ref();
+    <Number as Serialize>::serialize(&Number::from_str(&value.to_string()).unwrap(), serializer)
+}
+
+pub unsafe fn debug_fmt<TType, TLibfunc>(
+    f: &mut fmt::Formatter,
+    _id: &ConcreteTypeId,
+    _registry: &ProgramRegistry<TType, TLibfunc>,
+    ptr: NonNull<()>,
+    _info: &InfoOnlyConcreteType,
+) -> fmt::Result
+where
+    TType: GenericType,
+    TLibfunc: GenericLibfunc,
+    <TType as GenericType>::Concrete: ValueBuilder<TType, TLibfunc>,
+{
+    fmt::Debug::fmt(ptr.cast::<u16>().as_ref(), f)
+}
