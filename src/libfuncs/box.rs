78a7a6408e7250516f200d0e6db7c511cf216f5f
Edgar <git@edgarluque.com>
2023-06-29 17:37:52 +0200

    progress

--- src/libfuncs/box.rs
+++ src/libfuncs/box.rs
@@ -1,3 +1,34 @@
-//! # Boxing libfuncs
-//!
-//! TODO
+//! # Box libfuncs
+
+use super::{LibfuncBuilder, LibfuncHelper};
+use crate::{metadata::MetadataStorage, types::TypeBuilder};
+use cairo_lang_sierra::{
+    extensions::{boxing::BoxConcreteLibfunc, GenericLibfunc, GenericType},
+    program_registry::ProgramRegistry,
+};
+use melior::{
+    ir::{Block, Location},
+    Context,
+};
+
+/// Select and call the correct libfunc builder function from the selector.
+pub fn build<'ctx, 'this, TType, TLibfunc>(
+    context: &'ctx Context,
+    registry: &ProgramRegistry<TType, TLibfunc>,
+    entry: &'this Block<'ctx>,
+    location: Location<'ctx>,
+    helper: &LibfuncHelper<'ctx, 'this>,
+    metadata: &mut MetadataStorage,
+    selector: &BoxConcreteLibfunc,
+) -> Result<(), std::convert::Infallible>
+where
+    TType: GenericType,
+    TLibfunc: GenericLibfunc,
+    <TType as GenericType>::Concrete: TypeBuilder,
+    <TLibfunc as GenericLibfunc>::Concrete: LibfuncBuilder,
+{
+    match selector {
+        BoxConcreteLibfunc::Into(_) => todo!(),
+        BoxConcreteLibfunc::Unbox(_) => todo!(),
+    }
+}
