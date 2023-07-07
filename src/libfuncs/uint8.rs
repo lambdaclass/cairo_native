d069c275a188ba2bfc27232c94cb5d00f4459350
MrAzteca <azteca1998@users.noreply.github.com>
2023-07-07 13:05:00 +0200

    Add remaining uint tests and fix bugs. (#214)

--- src/libfuncs/uint8.rs
+++ src/libfuncs/uint8.rs
@@ -202,7 +202,7 @@ where
         location,
     ));
 
-    entry.append_operation(helper.cond_br(op0.result(0)?.into(), [0, 1], [&[]; 2], location));
+    entry.append_operation(helper.cond_br(op0.result(0)?.into(), [1, 0], [&[]; 2], location));
 
     Ok(())
 }
@@ -260,8 +260,8 @@ where
     <TType as GenericType>::Concrete: TypeBuilder<TType, TLibfunc, Error = CoreTypeBuilderError>,
     <TLibfunc as GenericLibfunc>::Concrete: LibfuncBuilder<TType, TLibfunc, Error = Error>,
 {
-    let lhs: Value = entry.argument(0)?.into();
-    let rhs: Value = entry.argument(1)?.into();
+    let lhs: Value = entry.argument(1)?.into();
+    let rhs: Value = entry.argument(2)?.into();
 
     let op = entry.append_operation(arith::divui(lhs, rhs, location));
     let result_div = op.result(0)?.into();
@@ -269,8 +269,11 @@ where
     let op = entry.append_operation(arith::remui(lhs, rhs, location));
     let result_rem = op.result(0)?.into();
 
-    entry.append_operation(helper.br(0, &[result_div, result_rem], location));
-
+    entry.append_operation(helper.br(
+        0,
+        &[entry.argument(0)?.into(), result_div, result_rem],
+        location,
+    ));
     Ok(())
 }
 
@@ -370,39 +373,69 @@ where
 
 #[cfg(test)]
 mod test {
-    use crate::utils::test::{load_cairo, run_program};
+    use crate::{
+        types::felt252::PRIME,
+        utils::test::{load_cairo, run_program},
+    };
     use cairo_lang_sierra::program::Program;
     use lazy_static::lazy_static;
+    use num_bigint::{BigInt, Sign};
     use serde_json::json;
+    use std::ops::Neg;
 
     lazy_static! {
-        static ref UINT_ADD: (String, Program) = load_cairo! {
+        static ref U8_OVERFLOWING_ADD: (String, Program) = load_cairo! {
             fn run_test(lhs: u8, rhs: u8) -> u8 {
                 lhs + rhs
             }
         };
-        static ref UINT_SUB: (String, Program) = load_cairo! {
+        static ref U8_OVERFLOWING_SUB: (String, Program) = load_cairo! {
             fn run_test(lhs: u8, rhs: u8) -> u8 {
                 lhs - rhs
             }
         };
+        static ref U8_SAFE_DIVMOD: (String, Program) = load_cairo! {
+            fn run_test(lhs: u8, rhs: u8) -> (u8, u8) {
+                let q = lhs / rhs;
+                let r = lhs % rhs;
+
+                (q, r)
+            }
+        };
+        static ref U8_EQUAL: (String, Program) = load_cairo! {
+            fn run_test(lhs: u8, rhs: u8) -> bool {
+                lhs == rhs
+            }
+        };
+        static ref U8_IS_ZERO: (String, Program) = load_cairo! {
+            use zeroable::IsZeroResult;
+
+            extern fn u8_is_zero(a: u8) -> IsZeroResult<u8> implicits() nopanic;
+
+            fn run_test(value: u8) -> bool {
+                match u8_is_zero(value) {
+                    IsZeroResult::Zero(_) => true,
+                    IsZeroResult::NonZero(_) => false,
+                }
+            }
+        };
     }
 
-    fn error_value(n: u128) -> serde_json::Value {
-        let mut bytes = [0u8; 32];
-        bytes[..16].copy_from_slice(&n.to_le_bytes());
+    // Parse numeric string into felt, wrapping negatives around the prime modulo.
+    fn f(value: &str) -> [u32; 8] {
+        let value = value.parse::<BigInt>().unwrap();
+        let value = match value.sign() {
+            Sign::Minus => &*PRIME - value.neg().to_biguint().unwrap(),
+            _ => value.to_biguint().unwrap(),
+        };
 
-        // The following transmute is safe because:
-        //   - It transmutes between a slice of `Copy`.
-        //   - Each element is a primitive which is valid no matter its underlying data.
-        //   - The slices have the same size.
-        //   - Their alignment is respected by using `transmute_copy()`.
-        let data = unsafe { std::mem::transmute_copy::<_, [u32; 8]>(&bytes) };
-        json!([(), [1, [[], [data]]]])
+        let mut u32_digits = value.to_u32_digits();
+        u32_digits.resize(8, 0);
+        u32_digits.try_into().unwrap()
     }
 
     #[test]
-    fn run_const_min() {
+    fn u8_const_min() {
         let program = load_cairo!(
             fn run_test() -> u8 {
                 0_u8
@@ -414,7 +447,7 @@ mod test {
     }
 
     #[test]
-    fn run_const_max() {
+    fn u8_const_max() {
         let program = load_cairo!(
             fn run_test() -> u8 {
                 255_u8
@@ -426,7 +459,7 @@ mod test {
     }
 
     #[test]
-    fn run_to_felt252() {
+    fn u8_to_felt252() {
         let program = load_cairo!(
             use traits::Into;
 
@@ -440,7 +473,7 @@ mod test {
     }
 
     #[test]
-    fn run_from_felt252() {
+    fn u8_from_felt252() {
         let program = load_cairo!(
             use traits::TryInto;
 
@@ -454,12 +487,12 @@ mod test {
     }
 
     #[test]
-    fn run_add() {
+    fn u8_overflowing_add() {
         fn run<const LHS: u8, const RHS: u8>() -> serde_json::Value {
-            run_program(&UINT_ADD, "run_test", json!([(), LHS, RHS]))
+            run_program(&U8_OVERFLOWING_ADD, "run_test", json!([(), LHS, RHS]))
         }
 
-        let add_error = 608642104203229548495787928534675319u128;
+        let add_error = f("608642104203229548495787928534675319");
 
         assert_eq!(run::<0, 0>(), json!([(), [0, [0]]]));
         assert_eq!(run::<0, 1>(), json!([(), [0, [1]]]));
@@ -469,45 +502,82 @@ mod test {
         assert_eq!(run::<1, 0>(), json!([(), [0, [1]]]));
         assert_eq!(run::<1, 1>(), json!([(), [0, [2]]]));
         assert_eq!(run::<1, 254>(), json!([(), [0, [255]]]));
-        assert_eq!(run::<1, 255>(), error_value(add_error));
+        assert_eq!(run::<1, 255>(), json!([(), [1, [[], [add_error]]]]));
 
         assert_eq!(run::<254, 0>(), json!([(), [0, [254]]]));
         assert_eq!(run::<254, 1>(), json!([(), [0, [255]]]));
-        assert_eq!(run::<254, 254>(), error_value(add_error));
-        assert_eq!(run::<254, 255>(), error_value(add_error));
+        assert_eq!(run::<254, 254>(), json!([(), [1, [[], [add_error]]]]));
+        assert_eq!(run::<254, 255>(), json!([(), [1, [[], [add_error]]]]));
 
         assert_eq!(run::<255, 0>(), json!([(), [0, [255]]]));
-        assert_eq!(run::<255, 1>(), error_value(add_error));
-        assert_eq!(run::<255, 254>(), error_value(add_error));
-        assert_eq!(run::<255, 255>(), error_value(add_error));
+        assert_eq!(run::<255, 1>(), json!([(), [1, [[], [add_error]]]]));
+        assert_eq!(run::<255, 254>(), json!([(), [1, [[], [add_error]]]]));
+        assert_eq!(run::<255, 255>(), json!([(), [1, [[], [add_error]]]]));
     }
 
     #[test]
-    fn run_sub() {
+    fn u8_overflowing_sub() {
         fn run<const LHS: u8, const RHS: u8>() -> serde_json::Value {
-            run_program(&UINT_SUB, "run_test", json!([(), LHS, RHS]))
+            run_program(&U8_OVERFLOWING_SUB, "run_test", json!([(), LHS, RHS]))
         }
 
-        let sub_error = 608642109794502019480482122260311927u128;
+        let sub_error = f("608642109794502019480482122260311927");
 
         assert_eq!(run::<0, 0>(), json!([(), [0, [0]]]));
-        assert_eq!(run::<0, 1>(), error_value(sub_error));
-        assert_eq!(run::<0, 254>(), error_value(sub_error));
-        assert_eq!(run::<0, 255>(), error_value(sub_error));
+        assert_eq!(run::<0, 1>(), json!([(), [1, [[], [sub_error]]]]));
+        assert_eq!(run::<0, 254>(), json!([(), [1, [[], [sub_error]]]]));
+        assert_eq!(run::<0, 255>(), json!([(), [1, [[], [sub_error]]]]));
 
         assert_eq!(run::<1, 0>(), json!([(), [0, [1]]]));
         assert_eq!(run::<1, 1>(), json!([(), [0, [0]]]));
-        assert_eq!(run::<1, 254>(), error_value(sub_error));
-        assert_eq!(run::<1, 255>(), error_value(sub_error));
+        assert_eq!(run::<1, 254>(), json!([(), [1, [[], [sub_error]]]]));
+        assert_eq!(run::<1, 255>(), json!([(), [1, [[], [sub_error]]]]));
 
         assert_eq!(run::<254, 0>(), json!([(), [0, [254]]]));
         assert_eq!(run::<254, 1>(), json!([(), [0, [253]]]));
         assert_eq!(run::<254, 254>(), json!([(), [0, [0]]]));
-        assert_eq!(run::<254, 255>(), error_value(sub_error));
+        assert_eq!(run::<254, 255>(), json!([(), [1, [[], [sub_error]]]]));
 
         assert_eq!(run::<255, 0>(), json!([(), [0, [255]]]));
         assert_eq!(run::<255, 1>(), json!([(), [0, [254]]]));
         assert_eq!(run::<255, 254>(), json!([(), [0, [1]]]));
         assert_eq!(run::<255, 255>(), json!([(), [0, [0]]]));
+    }
+
+    #[test]
+    fn u8_equal() {
+        let r = |lhs, rhs| run_program(&U8_EQUAL, "run_test", json!([lhs, rhs]));
+
+        assert_eq!(r(0, 0), json!([[1, []]]));
+        assert_eq!(r(0, 1), json!([[0, []]]));
+        assert_eq!(r(1, 0), json!([[0, []]]));
+        assert_eq!(r(1, 1), json!([[1, []]]));
+    }
+
+    #[test]
+    fn u8_is_zero() {
+        let r = |value| run_program(&U8_IS_ZERO, "run_test", json!([value]));
+
+        assert_eq!(r(0), json!([[1, []]]));
+        assert_eq!(r(1), json!([[0, []]]));
+    }
+
+    #[test]
+    fn u8_safe_divmod() {
+        let r = |lhs, rhs| run_program(&U8_SAFE_DIVMOD, "run_test", json!([(), lhs, rhs]));
+
+        let u8_is_zero = json!([f("32994284134408240")]);
+
+        assert_eq!(r(0, 0), json!([(), [1, [[], u8_is_zero]]]));
+        assert_eq!(r(0, 1), json!([(), [0, [[0u8, 0u8]]]]));
+        assert_eq!(r(0, 0xFFu8), json!([(), [0, [[0u8, 0u8]]]]));
+
+        assert_eq!(r(1, 0), json!([(), [1, [[], u8_is_zero]]]));
+        assert_eq!(r(1, 1), json!([(), [0, [[1u8, 0u8]]]]));
+        assert_eq!(r(1, 0xFFu8), json!([(), [0, [[0u8, 1u8]]]]));
+
+        assert_eq!(r(0xFFu8, 0), json!([(), [1, [[], u8_is_zero]]]));
+        assert_eq!(r(0xFFu8, 1), json!([(), [0, [[0xFFu8, 0u8]]]]));
+        assert_eq!(r(0xFFu8, 0xFFu8), json!([(), [0, [[1u8, 0u8]]]]));
     }
 }
