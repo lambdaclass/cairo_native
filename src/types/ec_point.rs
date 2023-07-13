2d1af5dec5bfadf0630ee907fff5d56c9b68e93a
Esteve Soler Arderiu <esteve.soler@lambdaclass.com>
2023-07-13 15:55:26 +0200

    Add `EcPoint` type.

--- src/types/ec_point.rs
+++ src/types/ec_point.rs
@@ -1,3 +1,40 @@
 //! # Elliptic curve point type
-//!
-//! TODO
+
+use super::TypeBuilder;
+use crate::{
+    error::types::{Error, Result},
+    metadata::MetadataStorage,
+};
+use cairo_lang_sierra::{
+    extensions::{types::InfoOnlyConcreteType, GenericLibfunc, GenericType},
+    program_registry::ProgramRegistry,
+};
+use melior::{
+    dialect::llvm,
+    ir::{r#type::IntegerType, Module, Type},
+    Context,
+};
+
+/// Build the MLIR type.
+///
+/// Check out [the module](self) for more info.
+pub fn build<'ctx, TType, TLibfunc>(
+    context: &'ctx Context,
+    _module: &Module<'ctx>,
+    _registry: &ProgramRegistry<TType, TLibfunc>,
+    _metadata: &mut MetadataStorage,
+    _info: &InfoOnlyConcreteType,
+) -> Result<Type<'ctx>>
+where
+    TType: GenericType,
+    TLibfunc: GenericLibfunc,
+    <TType as GenericType>::Concrete: TypeBuilder<TType, TLibfunc, Error = Error>,
+{
+    let felt252_ty = IntegerType::new(context, 252).into();
+
+    Ok(llvm::r#type::r#struct(
+        context,
+        &[felt252_ty, felt252_ty],
+        false,
+    ))
+}
