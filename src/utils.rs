4f34ba9d3ba395f86a28b1350e9a062013dded1a
Esteve Soler Arderiu <esteve.soler@fratilia.com>
2023-07-18 16:42:03 +0200

    Add workaround for LLVM `u128` alignment bug.

--- src/utils.rs
+++ src/utils.rs
@@ -32,7 +32,9 @@ pub fn get_integer_layout(width: u32) -> Layout {
     } else if width <= 64 {
         Layout::new::<u64>()
     } else if width <= 128 {
-        Layout::new::<u128>()
+        // Note: The manual alignment is there due to a bug in LLVM that generates inconsistent u128
+        //   alignment on at least the x86_64 architecture.
+        Layout::new::<u128>().align_to(16).unwrap()
     } else {
         Layout::array::<u64>(width.next_multiple_of(64) as usize >> 6).unwrap()
     }
