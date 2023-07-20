83026f9cc95cd9cd1ba5b2a92088f3c646ebfca8
Esteve Soler Arderiu <esteve.soler@lambdaclass.com>
2023-07-20 19:05:43 +0200

    Add `u256_is_zero` libfunc.

--- src/libfuncs/uint256.rs
+++ src/libfuncs/uint256.rs
@@ -19,7 +19,15 @@ use cairo_lang_sierra::{
     program_registry::ProgramRegistry,
 };
 use melior::{
-    ir::{Block, Location},
+    dialect::{
+        arith::{self, CmpiPredicate},
+        llvm,
+    },
+    ir::{
+        attribute::{DenseI64ArrayAttribute, IntegerAttribute},
+        r#type::IntegerType,
+        Block, Location,
+    },
     Context,
 };
 
@@ -60,7 +68,7 @@ pub fn build_divmod<'ctx, 'this, TType, TLibfunc>(
     _location: Location<'ctx>,
     _helper: &LibfuncHelper<'ctx, 'this>,
     _metadata: &mut MetadataStorage,
-    _selector: &SignatureOnlyConcreteLibfunc,
+    _info: &SignatureOnlyConcreteLibfunc,
 ) -> Result<()>
 where
     TType: GenericType,
@@ -73,13 +81,13 @@ where
 
 /// Generate MLIR operations for the `u256_is_zero` libfunc.
 pub fn build_is_zero<'ctx, 'this, TType, TLibfunc>(
-    _context: &'ctx Context,
+    context: &'ctx Context,
     _registry: &ProgramRegistry<TType, TLibfunc>,
-    _entry: &'this Block<'ctx>,
-    _location: Location<'ctx>,
-    _helper: &LibfuncHelper<'ctx, 'this>,
+    entry: &'this Block<'ctx>,
+    location: Location<'ctx>,
+    helper: &LibfuncHelper<'ctx, 'this>,
     _metadata: &mut MetadataStorage,
-    _selector: &SignatureOnlyConcreteLibfunc,
+    _info: &SignatureOnlyConcreteLibfunc,
 ) -> Result<()>
 where
     TType: GenericType,
@@ -87,7 +95,66 @@ where
     <TType as GenericType>::Concrete: TypeBuilder<TType, TLibfunc, Error = CoreTypeBuilderError>,
     <TLibfunc as GenericLibfunc>::Concrete: LibfuncBuilder<TType, TLibfunc, Error = Error>,
 {
-    todo!()
+    let i128_ty = IntegerType::new(context, 128).into();
+
+    let val_struct = entry.argument(0)?.into();
+    let val_lo = entry
+        .append_operation(llvm::extract_value(
+            context,
+            val_struct,
+            DenseI64ArrayAttribute::new(context, &[0]),
+            i128_ty,
+            location,
+        ))
+        .result(0)?
+        .into();
+    let val_hi = entry
+        .append_operation(llvm::extract_value(
+            context,
+            val_struct,
+            DenseI64ArrayAttribute::new(context, &[1]),
+            i128_ty,
+            location,
+        ))
+        .result(0)?
+        .into();
+
+    let k0 = entry
+        .append_operation(arith::constant(
+            context,
+            IntegerAttribute::new(0, i128_ty).into(),
+            location,
+        ))
+        .result(0)?
+        .into();
+    let val_lo_is_zero = entry
+        .append_operation(arith::cmpi(
+            context,
+            CmpiPredicate::Eq,
+            val_lo,
+            k0,
+            location,
+        ))
+        .result(0)?
+        .into();
+    let val_hi_is_zero = entry
+        .append_operation(arith::cmpi(
+            context,
+            CmpiPredicate::Eq,
+            val_hi,
+            k0,
+            location,
+        ))
+        .result(0)?
+        .into();
+
+    let val_is_zero = entry
+        .append_operation(arith::andi(val_lo_is_zero, val_hi_is_zero, location))
+        .result(0)?
+        .into();
+
+    entry.append_operation(helper.cond_br(val_is_zero, [0, 1], [&[], &[val_struct]], location));
+    Ok(())
 }
 
 /// Generate MLIR operations for the `u256_sqrt` libfunc.
@@ -98,7 +165,7 @@ pub fn build_square_root<'ctx, 'this, TType, TLibfunc>(
     _location: Location<'ctx>,
     _helper: &LibfuncHelper<'ctx, 'this>,
     _metadata: &mut MetadataStorage,
-    _selector: &SignatureOnlyConcreteLibfunc,
+    _info: &SignatureOnlyConcreteLibfunc,
 ) -> Result<()>
 where
     TType: GenericType,
@@ -107,4 +174,42 @@ where
     <TLibfunc as GenericLibfunc>::Concrete: LibfuncBuilder<TType, TLibfunc, Error = Error>,
 {
     todo!()
+}
+
+#[cfg(test)]
+mod test {
+    use crate::utils::test::{load_cairo, run_program};
+    use cairo_lang_sierra::program::Program;
+    use lazy_static::lazy_static;
+    use serde_json::json;
+
+    lazy_static! {
+        static ref U256_IS_ZERO: (String, Program) = load_cairo! {
+            use zeroable::IsZeroResult;
+            use debug::PrintTrait;
+
+            extern fn u256_is_zero(a: u256) -> IsZeroResult<u256> implicits() nopanic;
+
+            fn run_test(value: u256) -> bool {
+                core::integer::u128_to_felt252(value.low).print();
+                core::integer::u128_to_felt252(value.high).print();
+                match u256_is_zero(value) {
+                    IsZeroResult::Zero(_) => true,
+                    IsZeroResult::NonZero(_) => false,
+                }
+            }
+        };
+    }
+
+    #[test]
+    fn u256_is_zero() {
+        let r = |(value_hi, value_lo)| {
+            run_program(&U256_IS_ZERO, "run_test", json!([[value_lo, value_hi]]))
+        };
+
+        assert_eq!(r((0u128, 0u128)), json!([[1, []]]));
+        assert_eq!(r((0u128, 1u128)), json!([[0, []]]));
+        assert_eq!(r((1u128, 0u128)), json!([[0, []]]));
+        assert_eq!(r((1u128, 1u128)), json!([[0, []]]));
+    }
 }
