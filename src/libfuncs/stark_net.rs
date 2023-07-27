8ced273f9384c760aa5ba891fdb86b5aec090798
Esteve Soler Arderiu <esteve.soler@fratilia.com>
2023-07-27 17:19:43 +0200

    Add `contract_address_const` libfunc.

--- src/libfuncs/stark_net.rs
+++ src/libfuncs/stark_net.rs
@@ -66,7 +66,9 @@ where
             )
         }
         StarkNetConcreteLibfunc::ClassHashToFelt252(_) => todo!(),
-        StarkNetConcreteLibfunc::ContractAddressConst(_) => todo!(),
+        StarkNetConcreteLibfunc::ContractAddressConst(info) => {
+            build_contract_address_const(context, registry, entry, location, helper, metadata, info)
+        }
         StarkNetConcreteLibfunc::ContractAddressTryFromFelt252(info) => {
             build_contract_address_try_from_felt252(
                 context, registry, entry, location, helper, metadata, info,
@@ -578,6 +580,39 @@ where
         ],
         location,
     ));
+    Ok(())
+}
+
+pub fn build_contract_address_const<'ctx, 'this, TType, TLibfunc>(
+    context: &'ctx Context,
+    _registry: &ProgramRegistry<TType, TLibfunc>,
+    entry: &'this Block<'ctx>,
+    location: Location<'ctx>,
+    helper: &LibfuncHelper<'ctx, 'this>,
+    _metadata: &mut MetadataStorage,
+    info: &SignatureAndConstConcreteLibfunc,
+) -> Result<()>
+where
+    TType: GenericType,
+    TLibfunc: GenericLibfunc,
+    <TType as GenericType>::Concrete: TypeBuilder<TType, TLibfunc, Error = CoreTypeBuilderError>,
+    <TLibfunc as GenericLibfunc>::Concrete: LibfuncBuilder<TType, TLibfunc, Error = Error>,
+{
+    let value = match info.c.sign() {
+        Sign::Minus => (&info.c).neg().to_biguint().unwrap(),
+        _ => info.c.to_biguint().unwrap(),
+    };
+
+    let value = entry
+        .append_operation(arith::constant(
+            context,
+            Attribute::parse(context, &format!("{value} : i252")).unwrap(),
+            location,
+        ))
+        .result(0)?
+        .into();
+
+    entry.append_operation(helper.br(0, &[value], location));
     Ok(())
 }
 
