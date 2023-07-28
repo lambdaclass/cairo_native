b63160a13756e9b318c38171245be91ccb2a5ce8
Pablo Deymonnaz <deymonnaz@gmail.com>
2023-07-28 12:36:09 -0300

    test-erc20

---
+++ scripts/test-erc20.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Donwload the ERC20 contract from the Cairo repository and compile with the
+# Sierra compiler. The output is a MLIR file.
+
+curl https://raw.githubusercontent.com/starkware-libs/cairo/b4e049a13b62dc493ac378747af8e0908c1b86b7/crates/cairo-lang-starknet/test_data/erc20.sierra > erc20.sierra
+
+cargo run --profile=optimized-dev \
+    --features=build-cli,with-runtime \
+    --bin=sierra2mlir -- erc20.sierra
+
+rm erc20.sierra
