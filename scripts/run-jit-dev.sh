3a6d206a7d10b6cb289631d7f889e7ff5e57345c
Edgar Luque <git@edgarluque.com>
2023-08-07 12:44:56 +0200

    future proof

--- scripts/run-jit-dev.sh
+++ scripts/run-jit-dev.sh
@@ -2,4 +2,4 @@
 
 # Script to invoke the dev version of the jit runner to run programs easily.
 
-echo $3 | cargo r --profile optimized-dev --all-features --bin sierrajit $1 $2 --inputs - --outputs -
+echo $3 | cargo r --profile optimized-dev --all-features --bin cairo-native-run $1 $2 --inputs - --outputs -
