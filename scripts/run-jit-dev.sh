f7ef9a5d971b014568e9c0a399f38c539d82c25b
Edgar <git@edgarluque.com>
2023-08-08 15:02:43 +0200

    Add a Developer guide (#285)
    * start working on dev guide
    
    * progress
    
    * done
    
    * future proof
    
    * typo
    
    * proofread 1
    
    * typo

---
+++ scripts/run-jit-dev.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Script to invoke the dev version of the jit runner to run programs easily.
+
+echo $3 | cargo r --profile optimized-dev --all-features --bin cairo-native-run $1 $2 --inputs - --outputs -
