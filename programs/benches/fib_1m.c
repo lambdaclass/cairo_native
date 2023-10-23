1579d8dff001e6bec34ef63a76265770325760f9
Edgar Luque <git@edgarluque.com>
2023-10-23 17:30:54 +0200

    fix bench

---
+++ programs/benches/fib_1m.c
@@ -0,0 +1,34 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+
+
+typedef struct fib_return_values
+{
+    unsigned __int128 remaining_gas;
+    struct {
+        uint8_t discriminant;
+        union {
+            uint64_t ok[4];
+            struct {
+                void *ptr;
+                uint32_t len;
+                uint32_t cap;
+            } err;
+        };
+    } result;
+} fib_return_values_t;
+
+
+static void run_bench(fib_return_values_t *, void *, uint64_t)
+    __attribute__((weakref("_mlir_ciface_fib_1m::fib_1m::main")));
+
+
+int main()
+{
+    fib_return_values_t return_values;
+
+    run_bench(&return_values, NULL, 0);
+
+    return return_values.result.discriminant;
+}
