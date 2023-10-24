69b4e46a6ad9e08723375c38b08e53b7cb3317e6
Edgar Luque <git@edgarluque.com>
2023-10-24 13:26:01 +0200

    xport json too

--- scripts/bench-hyperfine.sh
+++ scripts/bench-hyperfine.sh
@@ -93,6 +93,7 @@ run_bench() {
     hyperfine \
         --warmup 3 \
         --export-markdown "$OUTPUT_DIR/$base_name.md" \
+        --export-json "$OUTPUT_DIR/$base_name.json" \
         -n "Cairo-vm (Rust, Cairo 1)" "$CAIRO_RUN --available-gas 18446744073709551615 -s $base_path.cairo" \
         -n "cairo-native (JIT MLIR ORC Engine)" "echo '[null, 18446744073709551615]' | $JIT_CLI $base_path.cairo $base_name::$base_name::main --inputs -" \
         -n "cairo-native (AOT Native binary)" "$OUTPUT_DIR/$base_name" \
