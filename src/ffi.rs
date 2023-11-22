ad3f59f1be9ecad2b0263a6e8069608c8b1b848e
Edgar Luque <git@edgarluque.com>
2023-11-22 14:44:30 +0100

    add ffi to translate to llvm ir

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,8 +1,17 @@
+use llvm_sys::prelude::{LLVMContextRef, LLVMModuleRef};
 use melior::ir::{Type, TypeLike};
+use mlir_sys::MlirOperation;
 use std::ffi::c_void;
 
 extern "C" {
     fn LLVMStructType_getFieldTypeAt(ty_ptr: *const c_void, index: u32) -> *const c_void;
+
+    /// Translate operation that satisfies LLVM dialect module requirements into an LLVM IR module living in the given context.
+    /// This translates operations from any dilalect that has a registered implementation of LLVMTranslationDialectInterface.
+    pub fn mlirTranslateModuleToLLVMIR(
+        module_operation_ptr: MlirOperation,
+        llvm_context: LLVMContextRef,
+    ) -> LLVMModuleRef;
 }
 
 /// For any `!llvm.struct<...>` type, return the MLIR type of the field at the requested index.
