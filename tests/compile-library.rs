ab179b13a0dcac5db742c49fdcc80a5aecf7aa6a
Edgar Luque <git@edgarluque.com>
2023-11-24 14:49:18 +0100

    fix ci

---
+++ tests/compile-library.rs
@@ -0,0 +1,85 @@
+use cairo_lang_compiler::{
+    compile_prepared_db, db::RootDatabase, diagnostics::DiagnosticsReporter,
+    project::setup_project, CompilerConfig,
+};
+use cairo_lang_sierra::{
+    extensions::core::{CoreLibfunc, CoreType},
+    program::Program,
+    program_registry::ProgramRegistry,
+    ProgramParser,
+};
+use cairo_lang_starknet::{
+    contract_class::compile_contract_in_prepared_db, inline_macros::selector::SelectorMacro,
+    plugin::StarkNetPlugin,
+};
+use cairo_native::metadata::{runtime_bindings::RuntimeBindingsMeta, MetadataStorage};
+use common::load_cairo;
+use melior::{
+    dialect::DialectRegistry,
+    ir::{Location, Module},
+    pass::{self, PassManager},
+    utility::{register_all_dialects, register_all_llvm_translations},
+    Context,
+};
+use std::{error::Error, path::Path};
+use tempfile::NamedTempFile;
+
+mod common;
+
+#[test]
+pub fn compile_library() -> Result<(), Box<dyn Error>> {
+    // Load the program.
+    let context = Context::new();
+
+    let program = load_cairo! {
+        fn run_test(lhs: felt252, rhs: felt252) -> felt252 {
+            lhs + rhs
+        }
+    };
+
+    // Initialize MLIR.
+    context.append_dialect_registry(&{
+        let registry = DialectRegistry::new();
+        register_all_dialects(&registry);
+        registry
+    });
+    context.load_all_available_dialects();
+    register_all_llvm_translations(&context);
+
+    // Compile the program.
+    let mut module = Module::new(Location::unknown(&context));
+    let mut metadata = MetadataStorage::new();
+    let registry = ProgramRegistry::<CoreType, CoreLibfunc>::new(&program.1)?;
+
+    // Make the runtime library available.
+    metadata.insert(RuntimeBindingsMeta::default()).unwrap();
+
+    cairo_native::compile::<CoreType, CoreLibfunc>(
+        &context,
+        &module,
+        &program.1,
+        &registry,
+        &mut metadata,
+        None,
+    )?;
+
+    // lower to llvm dialect
+    let pass_manager = PassManager::new(&context);
+    pass_manager.enable_verifier(true);
+    pass_manager.add_pass(pass::transform::create_canonicalizer());
+    pass_manager.add_pass(pass::conversion::create_scf_to_control_flow());
+    pass_manager.add_pass(pass::conversion::create_arith_to_llvm());
+    pass_manager.add_pass(pass::conversion::create_control_flow_to_llvm());
+    pass_manager.add_pass(pass::conversion::create_func_to_llvm());
+    pass_manager.add_pass(pass::conversion::create_index_to_llvm());
+    pass_manager.add_pass(pass::conversion::create_finalize_mem_ref_to_llvm());
+    pass_manager.add_pass(pass::conversion::create_reconcile_unrealized_casts());
+    pass_manager.run(&mut module)?;
+
+    let object = cairo_native::module_to_object(&module)?;
+
+    let file = NamedTempFile::new()?.into_temp_path();
+    cairo_native::object_to_shared_lib(&object, &file)?;
+
+    Ok(())
+}
