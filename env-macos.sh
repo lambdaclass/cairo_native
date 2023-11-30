89d038fc40ad0d19d8bfa7a6214d7a38c8e473f3
Jordi <jordi.bonet.valiente@lambdaclass.com>
2023-11-30 18:25:01 +0100

    Restore env

--- env-macos.sh
+++ env-macos.sh
@@ -4,9 +4,7 @@
 # It sets the LLVM environment variables.
 
 MLIR_SYS_170_PREFIX="$(brew --prefix llvm@17)"
-LLVM_SYS_170_PREFIX="$(brew --prefix llvm@17)"
 TABLEGEN_170_PREFIX="$(brew --prefix llvm@17)"
 
 export MLIR_SYS_170_PREFIX
 export TABLEGEN_170_PREFIX
-export LLVM_SYS_170_PREFIX
