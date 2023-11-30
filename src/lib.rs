a993d188fb69163c9a57ccda06a444ae8b40c9d7
Jordi <jordi.bonet.valiente@lambdaclass.com>
2023-11-30 17:39:04 +0100

    Format file

--- src/lib.rs
+++ src/lib.rs
@@ -20,34 +20,34 @@
 //! use cairo_native::executor::NativeExecutor;
 //! use cairo_native::values::JITValue;
 //! use std::path::Path;
-//! 
+//!
 //! fn main() {
 //!     let program_path = Path::new("programs/examples/hello.cairo");
 //!     // Compile the cairo program to sierra.
 //!     let sierra_program = cairo_native::utils::cairo_to_sierra(program_path);
-//! 
+//!
 //!     // Instantiate a Cairo Native MLIR context. This data structure is responsible for the MLIR
 //!     // initialization and compilation of sierra programs into a MLIR module.
 //!     let native_context = NativeContext::new();
-//! 
+//!
 //!     // Compile the sierra program into a MLIR module.
 //!     let native_program = native_context.compile(&sierra_program).unwrap();
-//! 
+//!
 //!     // The parameters of the entry point.
 //!     let params = &[JITValue::Felt252(Felt252::from_bytes_be(b"user"))];
-//! 
+//!
 //!     // Find the entry point id by its name.
 //!     let entry_point = "hello::hello::greet";
 //!     let entry_point_id = cairo_native::utils::find_function_id(&sierra_program, entry_point);
-//! 
+//!
 //!     // Instantiate the executor.
 //!     let native_executor = NativeExecutor::new(native_program);
-//! 
+//!
 //!     // Execute the program.
 //!     let result = native_executor
 //!         .execute(entry_point_id, params, None)
 //!         .unwrap();
-//! 
+//!
 //!     println!("Cairo program was compiled and executed successfully.");
 //!     println!("{:?}", result);
 //! }
