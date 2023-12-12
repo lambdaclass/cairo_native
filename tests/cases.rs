68d777cbddf9c866553269f3bab07ccb7d3b9b0a
fmoletta <99273364+fmoletta@users.noreply.github.com>
2023-12-12 11:29:01 +0200

    Implement `enum_snapshot_match` libfunc (#366)
    * Add test case that uses the enum_snapshot_match libfunc
    
    * Add missing test case
    
    * Add EnumSnapshotVariantsMeta to snapshot medatada
    
    * Implement enum_snapshot_match libfunc
    
    * Add comment
    
    * Group imports
    
    * Improve testing
    
    * Add file
    
    * Fix eof

--- tests/cases.rs
+++ tests/cases.rs
@@ -22,6 +22,9 @@ mod common;
 // TODO: compare error: Fail(Reason("assertion failed: `(left == right)` \n  left: `0`,\n right: `10` at tests/common.rs:453"))
 #[test_case("tests/cases/enums/enum_init.cairo")]
 #[test_case("tests/cases/enums/single_value.cairo")]
+#[test_case("tests/cases/enums/enum_match.cairo")]
+#[test_case("tests/cases/enums/enum_snapshot_match_a.cairo")]
+#[test_case("tests/cases/enums/enum_snapshot_match_b.cairo")]
 // returns
 #[test_case("tests/cases/returns/enums.cairo")]
 #[test_case("tests/cases/returns/simple.cairo")]
