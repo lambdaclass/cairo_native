b64c53625c96c9739543f9be9edfed144d6676a3
Edgar Luque <git@edgarluque.com>
2023-12-13 18:16:22 +0100

    clean

--- benches/libfuncs.rs
+++
@@ -1,8 +0,0 @@
-use criterion::{criterion_group, criterion_main, Criterion};
-
-pub fn criterion_benchmark(_c: &mut Criterion) {
-    // c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
-}
-
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
