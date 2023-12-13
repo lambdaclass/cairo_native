9c44f30bd401938a80a9442c35657910ff8f171b
Esteve Soler Arderiu <esteve.soler@lambdaclass.com>
2023-12-13 23:45:26 +0000

    Register debug utils on integration tests.

--- tests/common.rs
+++ tests/common.rs
@@ -264,6 +264,11 @@ pub fn run_native_program(
 
     #[cfg(feature = "with-runtime")]
     register_runtime_symbols(&engine);
+    #[cfg(feature = "with-debug-utils")]
+    metadata
+        .get::<cairo_native::metadata::debug_utils::DebugUtils>()
+        .unwrap()
+        .register_impls(&engine);
 
     cairo_native::execute(
         &engine,
