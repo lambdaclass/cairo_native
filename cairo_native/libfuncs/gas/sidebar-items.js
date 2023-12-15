eb8c9474a0cd316ad47089a6bd7c2495d316389a
edg-l <edg-l@users.noreply.github.com>
2023-12-15 17:46:36 +0000

    deploy: 1b4d1d55924760e92d95b6353acaa2f1aaf16400

--- cairo_native/libfuncs/gas/sidebar-items.js
+++ cairo_native/libfuncs/gas/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["build","build_builtin_withdraw_gas","build_get_builtin_costs","build_withdraw_gas"]};
+window.SIDEBAR_ITEMS = {"fn":["build","build_builtin_withdraw_gas","build_get_available_gas","build_get_builtin_costs","build_withdraw_gas"]};
