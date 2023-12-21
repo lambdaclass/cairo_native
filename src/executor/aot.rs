e84b0e458731cc48dbc840d4438fc012054cc8a9
Esteve Soler Arderiu <esteve.soler@fratilia.com>
2023-12-21 21:59:09 +0100

    Add shortcuts.

--- src/executor/aot.rs
+++ src/executor/aot.rs
@@ -2,8 +2,10 @@ use crate::{
     error::jit_engine::RunnerError,
     execution_result::{ContractExecutionResult, ExecutionResult},
     metadata::{gas::GasMetadata, syscall_handler::SyscallHandlerMeta},
+    module::NativeModule,
     utils::generate_function_name,
     values::JitValue,
+    OptLevel,
 };
 use cairo_lang_sierra::{
     extensions::core::{CoreLibfunc, CoreType},
@@ -14,6 +16,7 @@ use cairo_lang_sierra::{
 use libc::c_void;
 use libloading::Library;
 use starknet_types_core::felt::Felt;
+use tempfile::NamedTempFile;
 
 pub struct AotNativeExecutor {
     library: Library,
@@ -34,26 +37,44 @@ impl AotNativeExecutor {
             gas_metadata,
         }
     }
-}
 
-impl AotNativeExecutor {
+    /// Utility to convert a [`NativeModule`] into an [`AotNativeExecutor`].
+    pub fn from_native_module(module: NativeModule, opt_level: OptLevel) -> Self {
+        let NativeModule {
+            module,
+            registry,
+            mut metadata,
+        } = module;
+
+        let library_path = NamedTempFile::new().unwrap().into_temp_path();
+
+        let object_data = crate::module_to_object(&module, opt_level).unwrap();
+        crate::object_to_shared_lib(&object_data, &library_path).unwrap();
+
+        Self {
+            library: unsafe { Library::new(library_path).unwrap() },
+            registry,
+            gas_metadata: metadata.remove(),
+        }
+    }
+
     pub fn invoke_dynamic(
         &self,
         function_id: &FunctionId,
         args: &[JitValue],
         mut gas: Option<u128>,
         syscall_handler: Option<&SyscallHandlerMeta>,
-    ) -> ExecutionResult {
+    ) -> Result<ExecutionResult, RunnerError> {
         self.process_required_initial_gas(function_id, gas.as_mut());
 
-        super::invoke_dynamic(
+        Ok(super::invoke_dynamic(
             &self.registry,
             self.find_function_ptr(function_id),
             self.extract_signature(function_id),
             args,
             gas,
             syscall_handler.map(SyscallHandlerMeta::as_ptr),
-        )
+        ))
     }
 
     pub fn invoke_contract_dynamic(
@@ -82,7 +103,7 @@ impl AotNativeExecutor {
         ))
     }
 
-    fn find_function_ptr(&self, function_id: &FunctionId) -> *mut c_void {
+    pub fn find_function_ptr(&self, function_id: &FunctionId) -> *mut c_void {
         let function_name = generate_function_name(function_id);
         let function_name = format!("_mlir_ciface_{function_name}");
 
