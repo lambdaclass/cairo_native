f17e9bf4c2d6d9da1261d1b6e09a41797b879d3b
azteca1998 <azteca1998@users.noreply.github.com>
2024-01-31 13:57:56 +0000

    deploy: 21b076dcfc203c4ed68bb3345af096d73186f407

--- cairo_native/types/stark_net/sidebar-items.js
+++ cairo_native/types/stark_net/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["build","build_class_hash","build_contract_address","build_storage_address","build_storage_base_address","build_system"]};
+window.SIDEBAR_ITEMS = {"fn":["build","build_class_hash","build_contract_address","build_secp256_point","build_storage_address","build_storage_base_address","build_system"]};
