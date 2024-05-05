8dff3b7cac4c4611a5c16f86bd624c0fe1b0dc8b
Thomas Coratger <thomas.coratger@gmail.com>
2024-05-05 23:54:29 +0200

    Small refactor and util for OptLevel

--- src/ffi.rs
+++ src/ffi.rs
@@ -72,6 +72,39 @@ pub enum OptLevel {
     Aggressive,
 }
 
+impl From<usize> for OptLevel {
+    fn from(value: usize) -> Self {
+        match value {
+            0 => OptLevel::None,
+            1 => OptLevel::Less,
+            2 => OptLevel::Default,
+            _ => OptLevel::Aggressive,
+        }
+    }
+}
+
+impl Into<usize> for OptLevel {
+    fn into(self) -> usize {
+        match self {
+            OptLevel::None => 0,
+            OptLevel::Less => 1,
+            OptLevel::Default => 2,
+            OptLevel::Aggressive => 3,
+        }
+    }
+}
+
+impl From<u8> for OptLevel {
+    fn from(value: u8) -> Self {
+        match value {
+            0 => OptLevel::None,
+            1 => OptLevel::Less,
+            2 => OptLevel::Default,
+            _ => OptLevel::Aggressive,
+        }
+    }
+}
+
 /// Make sure to call
 pub fn module_to_object(
     module: &Module<'_>,
