48a23546b847126e696b20f73f12e7a554837571
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2024-05-14 23:11:43 +0200

    refactor test and increase cov (#591)

--- src/error.rs
+++ src/error.rs
@@ -1,12 +1,10 @@
 //! Various error types used thorough the crate.
+use crate::metadata::gas::GasMetadataError;
 use cairo_lang_sierra::{
     edit_state::EditStateError, ids::ConcreteTypeId, program_registry::ProgramRegistryError,
 };
-
 use std::{alloc::LayoutError, num::TryFromIntError};
 use thiserror::Error;
-
-use crate::metadata::gas::GasMetadataError;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
@@ -64,15 +62,43 @@ pub enum Error {
     ConstDataMismatch,
 }
 
-pub fn make_unexpected_value_error(expected: String) -> Error {
-    Error::UnexpectedValue(expected)
+impl Error {
+    pub fn make_missing_parameter(ty: &ConcreteTypeId) -> Self {
+        Self::MissingParameter(
+            ty.debug_name
+                .as_ref()
+                .map(|x| x.to_string())
+                .unwrap_or_default(),
+        )
+    }
 }
 
-pub fn make_missing_parameter(ty: &ConcreteTypeId) -> Error {
-    Error::MissingParameter(
-        ty.debug_name
-            .as_ref()
-            .map(|x| x.to_string())
-            .unwrap_or_default(),
-    )
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_make_missing_parameter() {
+        // Test with a type ID that has a debug name
+        let ty_with_debug_name = ConcreteTypeId {
+            debug_name: Some("u32".into()),
+            id: 10,
+        };
+
+        assert_eq!(
+            Error::make_missing_parameter(&ty_with_debug_name).to_string(),
+            "missing parameter of type 'u32'"
+        );
+
+        // Test with a type ID that does not have a debug name
+        let ty_without_debug_name = ConcreteTypeId {
+            debug_name: None,
+            id: 10,
+        };
+
+        assert_eq!(
+            Error::make_missing_parameter(&ty_without_debug_name).to_string(),
+            "missing parameter of type ''"
+        );
+    }
 }
