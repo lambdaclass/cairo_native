2d0d41534402562347e191370cddc6fd4d4496f8
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-07-29 18:57:36 -0300

    Add test for failed case

--- src/libfuncs/array.rs
+++ src/libfuncs/array.rs
@@ -2024,4 +2024,42 @@ mod test {
             )
         );
     }
+
+    #[test]
+    fn snapshot_failed_multi_pop_front() {
+        let program = load_cairo!(
+            use array::ArrayTrait;
+
+            fn run_test() -> Span<felt252> {
+                let mut numbers = array![1, 2].span();
+
+                // should fail (return none)
+                if numbers.multi_pop_front::<3>().is_some() {
+                    panic!();
+                }
+
+                numbers
+            }
+        );
+
+        let result = run_program(&program, "run_test", &[]).return_value;
+
+        assert_eq!(
+            result,
+            // Panic result
+            jit_enum!(
+                0,
+                jit_struct!(
+                    // Tuple
+                    jit_struct!(
+                        // Span of original array
+                        jit_struct!(JitValue::Array(vec![
+                            JitValue::Felt252(1.into()),
+                            JitValue::Felt252(2.into()),
+                        ])),
+                    )
+                )
+            )
+        );
+    }
 }
