d0490a229ab1aad30a29432df5f7a4111c6ddc57
Edgar Luque <git@edgarluque.com>
2024-10-11 16:20:31 +0200

    oops

--- src/libfuncs/felt252.rs
+++ src/libfuncs/felt252.rs
@@ -100,7 +100,7 @@ pub fn build_binary_operation<'ctx, 'this>(
         Felt252BinaryOperator::Add => {
             let lhs = entry.append_op_result(arith::extui(lhs, i256, location))?;
             let rhs = entry.append_op_result(arith::extui(rhs, i256, location))?;
-            let result = entry.append_op_result(arith::subi(lhs, rhs, location))?;
+            let result = entry.append_op_result(arith::addi(lhs, rhs, location))?;
 
             let prime = entry.const_int_from_type(context, location, PRIME.clone(), i256)?;
             let result_mod = entry.append_op_result(arith::subi(result, prime, location))?;
