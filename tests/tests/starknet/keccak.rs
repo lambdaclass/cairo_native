1ecc4199f03824b62a3e9488efda39ef2e8497c4
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-10-21 12:14:12 -0300

    Add test

--- tests/tests/starknet/keccak.rs
+++ tests/tests/starknet/keccak.rs
@@ -1,7 +1,8 @@
 use crate::common::run_native_starknet_contract;
 use cairo_lang_compiler::CompilerConfig;
+use cairo_lang_sierra::ids::FunctionId;
 use cairo_lang_starknet::compile::compile_path;
-use cairo_native::starknet_stub::StubSyscallHandler;
+use cairo_native::{executor::AotContractExecutor, starknet_stub::StubSyscallHandler};
 use lazy_static::lazy_static;
 use std::path::Path;
 
@@ -34,6 +35,31 @@ fn keccak_test() {
         &[],
         &mut StubSyscallHandler::default(),
     );
+
+    assert!(!result.failure_flag);
+    assert_eq!(
+        result.remaining_gas,
+        340282366920938463463374607431768143515
+    );
+    assert_eq!(result.return_values, vec![1.into()]);
+}
+
+#[test]
+fn keccak_test_contract_executor() {
+    let contract = &KECCAK_CONTRACT;
+
+    let entry_point = contract.entry_points_by_type.external.first().unwrap();
+
+    let program = contract.extract_sierra_program().unwrap();
+    let executor = AotContractExecutor::new(&program, cairo_native::OptLevel::Default).unwrap();
+    let result = executor
+        .run(
+            &FunctionId::new(entry_point.function_idx as u64),
+            &[],
+            u128::MAX.into(),
+            &mut StubSyscallHandler::default(),
+        )
+        .unwrap();
 
     assert!(!result.failure_flag);
     assert_eq!(
