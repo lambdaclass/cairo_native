1ebd0df646d61f6cb7e6c10967c9d7c7ce9e5135
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-11-08 10:49:31 -0300

    Add linear_search.c

---
+++ programs/benches/linear_search.c
@@ -0,0 +1,32 @@
+#include <assert.h>
+#include <stdint.h>
+
+
+typedef struct linear_search_return_values
+{
+    uint64_t range_check_counter;
+    unsigned __int128 remaining_gas;
+    struct {
+        uint8_t discriminant;
+        struct {
+            void* ptr;
+            uint32_t len;
+            uint32_t cap;
+        } err;
+    } result;
+} linear_search_return_values_t;
+
+
+static void run_bench(linear_search_return_values_t*, uint64_t)
+__attribute__((weakref("_mlir_ciface_linear_search::linear_search::main(f1)")));
+
+
+int main()
+{
+    linear_search_return_values_t return_values;
+
+    run_bench(&return_values, 0);
+    assert(return_values.result.discriminant == 0);
+
+    return 0;
+}
