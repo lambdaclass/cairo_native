356b47ab3b8f2aed9e3f4589bec5997bc763e8ae
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-11-11 13:33:05 -0300

    Add builtin

--- programs/benches/linear_search.c
+++ programs/benches/linear_search.c
@@ -18,11 +18,16 @@ typedef struct linear_search_return_values
 
 
 static void run_bench(linear_search_return_values_t*, uint64_t)
-__attribute__((weakref("_mlir_ciface_linear_search::linear_search::main(f1)")));
+    __attribute__((weakref("_mlir_ciface_linear_search::linear_search::main(f1)")));
 
+extern uint64_t* cairo_native__set_costs_builtin(uint64_t*);
 
 int main()
 {
+    uint64_t BuiltinCosts[7] = {1, 4050, 583, 4085, 491, 230, 604};
+
+    cairo_native__set_costs_builtin(&BuiltinCosts[0]);
+
     linear_search_return_values_t return_values;
 
     run_bench(&return_values, 0);
