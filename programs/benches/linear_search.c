7ba0be1718f62b7e1781fc557c0e7e5c38eafe27
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-11-11 14:25:53 -0300

    Fix

--- programs/benches/linear_search.c
+++ programs/benches/linear_search.c
@@ -9,15 +9,15 @@ typedef struct linear_search_return_values
     struct {
         uint8_t discriminant;
         struct {
-            void* ptr;
-            uint32_t len;
+            void *ptr;
+            uint32_t start;
+            uint32_t end;
             uint32_t cap;
         } err;
     } result;
 } linear_search_return_values_t;
 
-
-static void run_bench(linear_search_return_values_t*, uint64_t)
+static void run_bench(linear_search_return_values_t *, uint64_t)
     __attribute__((weakref("_mlir_ciface_linear_search::linear_search::main(f1)")));
 
 extern uint64_t* cairo_native__set_costs_builtin(uint64_t*);
@@ -31,7 +31,7 @@ int main()
     linear_search_return_values_t return_values;
 
     run_bench(&return_values, 0);
-    assert(return_values.result.discriminant == 0);
+    assert((return_values.result.discriminant & 0x1) == 0);
 
     return 0;
 }
