dc67a304435464358fa917ef153572897449165d
FrancoGiachetta <francogiachetta27@gmail.com>
2024-11-13 11:53:42 -0300

    clippy

--- src/libfuncs.rs
+++ src/libfuncs.rs
@@ -275,12 +275,12 @@ where
 {
     #[doc(hidden)]
     pub(crate) fn results(self) -> impl Iterator<Item = NativeResult<Vec<Value<'ctx, 'this>>>> {
-        self.results.into_iter().enumerate().map(|(_, x)| {
+        self.results.into_iter().enumerate().map(|(branch_idx, x)| {
             x.into_iter()
                 .enumerate()
-                .map(|(_, x)| {
+                .map(|(arg_idx, x)| {
                     x.into_inner().to_native_assert_error(
-                        "Argument #{arg_idx} of branch {branch_idx} doesn't have a value.",
+                       &format!("Argument #{arg_idx} of branch {branch_idx} doesn't have a value."),
                     )
                 })
                 .collect()
