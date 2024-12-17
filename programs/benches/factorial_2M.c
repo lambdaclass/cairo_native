12d1e8798f16a0b4dd4ba914660a4b7ae1a3cb7e
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-12-17 14:45:30 -0300

    Add RangeCheck as first argument to `run_bench`

--- programs/benches/factorial_2M.c
+++ programs/benches/factorial_2M.c
@@ -17,7 +17,7 @@ typedef struct factorial_return_values
     } result;
 } factorial_return_values_t;
 
-static void run_bench(factorial_return_values_t*, uint64_t)
+static void run_bench(factorial_return_values_t*, uint64_t, uint64_t)
 __attribute__((weakref("_mlir_ciface_factorial_2M::factorial_2M::main(f1)")));
 
 extern uint64_t* cairo_native__set_costs_builtin(uint64_t*);
@@ -30,7 +30,7 @@ int main()
 
     cairo_native__set_costs_builtin(&BuiltinCosts[0]);
 
-    run_bench(&return_values, 0xFFFFFFFFFFFFFFFF);
+    run_bench(&return_values, 0, 0xFFFFFFFFFFFFFFFF);
     assert((return_values.result.discriminant & 0x1) == 0);
 
     return 0;
