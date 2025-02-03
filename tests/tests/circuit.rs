2f2cff3396d2c4b4c7f8a4c2251109224499f722
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-02-03 18:58:10 -0300

    Add more tests

--- tests/tests/circuit.rs
+++ tests/tests/circuit.rs
@@ -3,6 +3,7 @@ use crate::common::{load_cairo, run_native_program, run_vm_program};
 use cairo_lang_runner::SierraCasmRunner;
 use cairo_lang_sierra::program::Program;
 use cairo_native::starknet::DummySyscallHandler;
+use cairo_native::Value;
 use lazy_static::lazy_static;
 
 lazy_static! {
@@ -14,7 +15,7 @@ lazy_static! {
             CircuitInputs, AddInputResultTrait
         };
 
-        fn test() {
+        fn test_guarantee_first_limb() {
             let in1 = CircuitElement::<CircuitInput<0>> {};
             let in2 = CircuitElement::<CircuitInput<1>> {};
             let add = circuit_add(in1, in2);
@@ -33,26 +34,151 @@ lazy_static! {
 
             assert!(outputs.get_output(mul) == u384 { limb0: 6, limb1: 0, limb2: 0, limb3: 0 });
         }
+
+        fn test_guarantee_last_limb() {
+            let in1 = CircuitElement::<CircuitInput<0>> {};
+            let in2 = CircuitElement::<CircuitInput<1>> {};
+            let add = circuit_add(in1, in2);
+
+            let modulus = TryInto::<_, CircuitModulus>::try_into([7, 0, 0, 1]).unwrap();
+            let outputs = (add,)
+                .new_inputs()
+                .next([5, 0, 0, 0])
+                .next([9, 0, 0, 0])
+                .done()
+                .eval(modulus)
+                .unwrap();
+
+            assert!(outputs.get_output(add) == u384 { limb0: 14, limb1: 0, limb2: 0, limb3: 0 });
+        }
+
+        fn test_guarantee_middle_limb() {
+            let in1 = CircuitElement::<CircuitInput<0>> {};
+            let in2 = CircuitElement::<CircuitInput<1>> {};
+            let add = circuit_add(in1, in2);
+
+            let modulus = TryInto::<_, CircuitModulus>::try_into([7, 0, 1, 0]).unwrap();
+            let outputs = (add,)
+                .new_inputs()
+                .next([5, 0, 0, 0])
+                .next([9, 0, 0, 0])
+                .done()
+                .eval(modulus)
+                .unwrap();
+
+            assert!(outputs.get_output(add) == u384 { limb0: 14, limb1: 0, limb2: 0, limb3: 0 });
+        }
     };
 }
 
 #[test]
-fn circuit() {
+fn circuit_guarantee_first_limb() {
     let program = &TEST;
 
-    let result_vm = run_vm_program(program, "test", vec![], Some(DEFAULT_GAS as usize)).unwrap();
+    let result_vm = run_vm_program(
+        program,
+        "test_guarantee_first_limb",
+        vec![],
+        Some(DEFAULT_GAS as usize),
+    )
+    .unwrap();
 
     let result_native = run_native_program(
         program,
-        "test",
+        "test_guarantee_first_limb",
         &[],
         Some(DEFAULT_GAS),
         Option::<DummySyscallHandler>::None,
     );
 
+    assert!(matches!(
+        result_native.return_value,
+        Value::Enum { tag: 0, .. }
+    ));
+
     compare_outputs(
         &program.1,
-        &program.2.find_function("test").unwrap().id,
+        &program
+            .2
+            .find_function("test_guarantee_first_limb")
+            .unwrap()
+            .id,
+        &result_vm,
+        &result_native,
+    )
+    .unwrap();
+}
+
+#[test]
+fn circuit_guarantee_last_limb() {
+    let program = &TEST;
+
+    let result_vm = run_vm_program(
+        program,
+        "test_guarantee_last_limb",
+        vec![],
+        Some(DEFAULT_GAS as usize),
+    )
+    .unwrap();
+
+    let result_native = run_native_program(
+        program,
+        "test_guarantee_last_limb",
+        &[],
+        Some(DEFAULT_GAS),
+        Option::<DummySyscallHandler>::None,
+    );
+
+    assert!(matches!(
+        result_native.return_value,
+        Value::Enum { tag: 0, .. }
+    ));
+
+    compare_outputs(
+        &program.1,
+        &program
+            .2
+            .find_function("test_guarantee_last_limb")
+            .unwrap()
+            .id,
+        &result_vm,
+        &result_native,
+    )
+    .unwrap();
+}
+
+#[test]
+fn circuit_guarantee_middle_limb() {
+    let program = &TEST;
+
+    let result_vm = run_vm_program(
+        program,
+        "test_guarantee_middle_limb",
+        vec![],
+        Some(DEFAULT_GAS as usize),
+    )
+    .unwrap();
+
+    let result_native = run_native_program(
+        program,
+        "test_guarantee_middle_limb",
+        &[],
+        Some(DEFAULT_GAS),
+        Option::<DummySyscallHandler>::None,
+    );
+
+    assert!(matches!(
+        result_native.return_value,
+        Value::Enum { tag: 0, .. }
+    ));
+
+    compare_outputs(
+        &program.1,
+        &program
+            .2
+            .find_function("test_guarantee_middle_limb")
+            .unwrap()
+            .id,
         &result_vm,
         &result_native,
     )
