fa14c0a79a062a53c12bef3db773f08494df14e5
FrancoGiachetta <francogiachetta27@gmail.com>
2025-03-10 10:32:15 -0300

    add type builder for blake

---
+++ src/libfuncs/blake.rs
@@ -0,0 +1,61 @@
+use cairo_lang_sierra::{
+    extensions::{
+        blake::BlakeConcreteLibfunc,
+        core::{CoreLibfunc, CoreType},
+        lib_func::SignatureOnlyConcreteLibfunc,
+    },
+    program_registry::ProgramRegistry,
+};
+use melior::{
+    ir::{Block, Location},
+    Context,
+};
+
+use crate::{error::Result, metadata::MetadataStorage};
+
+use super::LibfuncHelper;
+
+pub fn build<'ctx, 'this>(
+    context: &'ctx Context,
+    registry: &ProgramRegistry<CoreType, CoreLibfunc>,
+    entry: &'this Block<'ctx>,
+    location: Location<'ctx>,
+    helper: &LibfuncHelper<'ctx, 'this>,
+    metadata: &mut MetadataStorage,
+    selector: &BlakeConcreteLibfunc,
+) -> Result<()> {
+    match selector {
+        BlakeConcreteLibfunc::Blake2sCompress(info) => {
+            build_blake_compress(context, registry, entry, location, helper, metadata, info)?
+        }
+        BlakeConcreteLibfunc::Blake2sFinalize(info) => {
+            build_blake_finalize(context, registry, entry, location, helper, metadata, info)?
+        }
+    }
+
+    Ok(())
+}
+
+fn build_blake_compress<'ctx, 'this>(
+    context: &'ctx Context,
+    registry: &ProgramRegistry<CoreType, CoreLibfunc>,
+    entry: &'this Block<'ctx>,
+    location: Location<'ctx>,
+    helper: &LibfuncHelper<'ctx, 'this>,
+    metadata: &mut MetadataStorage,
+    info: &SignatureOnlyConcreteLibfunc,
+) -> Result<()> {
+    Ok(())
+}
+
+fn build_blake_finalize<'ctx, 'this>(
+    context: &'ctx Context,
+    registry: &ProgramRegistry<CoreType, CoreLibfunc>,
+    entry: &'this Block<'ctx>,
+    location: Location<'ctx>,
+    helper: &LibfuncHelper<'ctx, 'this>,
+    metadata: &mut MetadataStorage,
+    info: &SignatureOnlyConcreteLibfunc,
+) -> Result<()> {
+    Ok(())
+}
