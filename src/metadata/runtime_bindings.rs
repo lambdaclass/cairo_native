583fcf2aef329dc317e6260ce5061311203a578f
Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
2025-04-01 17:13:06 -0300

    Improve gas documentation (#1159)
    * Add GasCost and BuiltinCost documentation
    
    * Add utility function for obtaining builtin cost index
    
    * Add doc comment explaining the content of the GasCost metadata entry
    
    * Add utility function `build_actual_gas_cost` to remove duplicated code
    
    * Improve gas builtin doc
    
    * Update mentions to gas builtin, that actually refer to costs builtin
    
    * Rename function to calculate_gas_cost
    
    ---------
    
    Co-authored-by: Franco Giachetta <francogiachetta27@gmail.com>

--- src/metadata/runtime_bindings.rs
+++ src/metadata/runtime_bindings.rs
@@ -39,7 +39,7 @@ enum RuntimeBinding {
     DictGasRefund,
     DictDrop,
     DictDup,
-    GetGasBuiltin,
+    GetCostsBuiltin,
     DebugPrint,
     #[cfg(feature = "with-cheatcode")]
     VtableCheatcode,
@@ -64,7 +64,7 @@ impl RuntimeBinding {
             RuntimeBinding::DictGasRefund => "cairo_native__dict_gas_refund",
             RuntimeBinding::DictDrop => "cairo_native__dict_drop",
             RuntimeBinding::DictDup => "cairo_native__dict_dup",
-            RuntimeBinding::GetGasBuiltin => "cairo_native__get_costs_builtin",
+            RuntimeBinding::GetCostsBuiltin => "cairo_native__get_costs_builtin",
             #[cfg(feature = "with-cheatcode")]
             RuntimeBinding::VtableCheatcode => "cairo_native__vtable_cheatcode",
         }
@@ -106,7 +106,7 @@ impl RuntimeBinding {
             }
             RuntimeBinding::DictDrop => crate::runtime::cairo_native__dict_drop as *const (),
             RuntimeBinding::DictDup => crate::runtime::cairo_native__dict_dup as *const (),
-            RuntimeBinding::GetGasBuiltin => {
+            RuntimeBinding::GetCostsBuiltin => {
                 crate::runtime::cairo_native__get_costs_builtin as *const ()
             }
             #[cfg(feature = "with-cheatcode")]
@@ -599,9 +599,9 @@ impl RuntimeBindingsMeta {
         ))
     }
 
-    // Register if necessary, then invoke the `set_gas_builtin()` function.
+    // Register if necessary, then invoke the `get_costs_builtin()` function.
     #[allow(clippy::too_many_arguments)]
-    pub fn get_gas_builtin<'c, 'a>(
+    pub fn get_costs_builtin<'c, 'a>(
         &mut self,
         context: &'c Context,
         module: &Module,
@@ -616,7 +616,7 @@ impl RuntimeBindingsMeta {
             module,
             block,
             location,
-            RuntimeBinding::GetGasBuiltin,
+            RuntimeBinding::GetCostsBuiltin,
         )?;
 
         Ok(block.append_operation(
@@ -680,7 +680,7 @@ pub fn setup_runtime(find_symbol_ptr: impl Fn(&str) -> Option<*mut c_void>) {
         RuntimeBinding::DictGasRefund,
         RuntimeBinding::DictDrop,
         RuntimeBinding::DictDup,
-        RuntimeBinding::GetGasBuiltin,
+        RuntimeBinding::GetCostsBuiltin,
         RuntimeBinding::DebugPrint,
         #[cfg(feature = "with-cheatcode")]
         RuntimeBinding::VtableCheatcode,
