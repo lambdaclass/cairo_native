08301ea5e8f458bee2612b2b982389d6dc4ff346
FrancoGiachetta <francogiachetta27@gmail.com>
2025-04-03 18:14:26 -0300

    pack, const, is_zero, sub (not working yet)

--- src/libfuncs/qm31.rs
+++ src/libfuncs/qm31.rs
@@ -2,21 +2,28 @@ use cairo_lang_sierra::{
     extensions::{
         core::{CoreLibfunc, CoreType},
         lib_func::SignatureOnlyConcreteLibfunc,
-        qm31::{QM31BinaryOpConcreteLibfunc, QM31Concrete, QM31ConstConcreteLibfunc},
+        qm31::{
+            QM31BinaryOpConcreteLibfunc, QM31BinaryOperator, QM31Concrete, QM31ConstConcreteLibfunc,
+        },
     },
     program_registry::ProgramRegistry,
 };
 use melior::{
-    ir::{Block, Location},
+    dialect::arith::{self, CmpiPredicate},
+    ir::{r#type::IntegerType, Block, BlockLike, Location},
     Context,
 };
+use num_bigint::BigInt;
 
-use crate::{error::Result, metadata::MetadataStorage};
+use crate::{error::Result, metadata::MetadataStorage, utils::BlockExt};
 
 use super::LibfuncHelper;
 
-const M31_SIZE: u32 = 36;
-const M31_MAX: u64 = 1 << M31_SIZE;
+// An M31 is the quarter part of a QM31, is a 36-bit integer
+// whose 5 least significant bits are 0s.
+const M31_SIZE: u8 = 36;
+const M31_MAX: u128 = 1 << M31_SIZE;
+const QM31_PRIME: u128 = (1 << 31) - 1;
 
 /// Select and call the correct libfunc builder function from the selector.
 pub fn build<'ctx, 'this>(
@@ -33,13 +40,13 @@ pub fn build<'ctx, 'this>(
             build_qm31_pack(context, registry, entry, location, helper, metadata, info)
         }
         QM31Concrete::Unpack(info) => {
-            build_qm31_unpack(context, registry, entry, location, helper, metadata, info)
+            todo!("impl qm31_unpack");
         }
         QM31Concrete::Const(info) => {
             build_qm31_const(context, registry, entry, location, helper, metadata, info)
         }
         QM31Concrete::FromM31(info) => {
-            build_qm31_from_m31(context, registry, entry, location, helper, metadata, info)
+            todo!("impl qm31_from_m31");
         }
         QM31Concrete::IsZero(info) => {
             build_qm31_is_zero(context, registry, entry, location, helper, metadata, info)
@@ -50,80 +57,143 @@ pub fn build<'ctx, 'this>(
     }
 }
 
-/// Select and call the correct libfunc builder function from the selector.
 pub fn build_qm31_pack<'ctx, 'this>(
     context: &'ctx Context,
-    registry: &ProgramRegistry<CoreType, CoreLibfunc>,
+    _registry: &ProgramRegistry<CoreType, CoreLibfunc>,
     entry: &'this Block<'ctx>,
     location: Location<'ctx>,
     helper: &LibfuncHelper<'ctx, 'this>,
-    metadata: &mut MetadataStorage,
-    info: &SignatureOnlyConcreteLibfunc,
+    _metadata: &mut MetadataStorage,
+    _info: &SignatureOnlyConcreteLibfunc,
 ) -> Result<()> {
+    let i144_ty = IntegerType::new(context, 144).into();
+    let m31_max = entry.const_int(context, location, M31_MAX, 144)?;
+
+    let m31_0 = entry.arg(0)?;
+    let m31_1 = entry.arg(1)?;
+    let m31_2 = entry.arg(2)?;
+    let m31_3 = entry.arg(3)?;
+
+    // Extend every limb to 144 bits
+    let m31_0 = entry.extui(m31_0, i144_ty, location)?;
+    let m31_1 = entry.extui(m31_1, i144_ty, location)?;
+    let m31_2 = entry.extui(m31_2, i144_ty, location)?;
+
+    // We first crate the qm31 with its most significant bits
+    let qm31 = entry.extui(m31_3, i144_ty, location)?;
+
+    let qm31 = entry.shli(qm31, m31_max, location)?;
+    let qm31 = entry.addi(qm31, m31_2, location)?;
+
+    let qm31 = entry.shli(qm31, m31_max, location)?;
+    let qm31 = entry.addi(qm31, m31_1, location)?;
+
+    let qm31 = entry.shli(qm31, m31_max, location)?;
+    let qm31 = entry.addi(qm31, m31_0, location)?;
+
+    entry.append_operation(helper.br(0, &[qm31], location));
+
     Ok(())
 }
 
-/// Select and call the correct libfunc builder function from the selector.
-pub fn build_qm31_unpack<'ctx, 'this>(
-    context: &'ctx Context,
-    registry: &ProgramRegistry<CoreType, CoreLibfunc>,
-    entry: &'this Block<'ctx>,
-    location: Location<'ctx>,
-    helper: &LibfuncHelper<'ctx, 'this>,
-    metadata: &mut MetadataStorage,
-    info: &SignatureOnlyConcreteLibfunc,
-) -> Result<()> {
-    Ok(())
-}
-
-/// Select and call the correct libfunc builder function from the selector.
 pub fn build_qm31_const<'ctx, 'this>(
     context: &'ctx Context,
-    registry: &ProgramRegistry<CoreType, CoreLibfunc>,
+    _registry: &ProgramRegistry<CoreType, CoreLibfunc>,
     entry: &'this Block<'ctx>,
     location: Location<'ctx>,
     helper: &LibfuncHelper<'ctx, 'this>,
-    metadata: &mut MetadataStorage,
+    _metadata: &mut MetadataStorage,
     info: &QM31ConstConcreteLibfunc,
 ) -> Result<()> {
+    let mut qm31 = BigInt::from(info.w3);
+    qm31 <<= M31_MAX;
+    qm31 += BigInt::from(info.w2);
+    qm31 <<= M31_MAX;
+    qm31 = BigInt::from(info.w1);
+    qm31 <<= M31_MAX;
+    qm31 = BigInt::from(info.w0);
+
+    let qm31 = entry.const_int(context, location, qm31, 144)?;
+
+    entry.append_operation(helper.br(0, &[qm31], location));
+
     Ok(())
 }
 
-/// Select and call the correct libfunc builder function from the selector.
-pub fn build_qm31_from_m31<'ctx, 'this>(
-    context: &'ctx Context,
-    registry: &ProgramRegistry<CoreType, CoreLibfunc>,
-    entry: &'this Block<'ctx>,
-    location: Location<'ctx>,
-    helper: &LibfuncHelper<'ctx, 'this>,
-    metadata: &mut MetadataStorage,
-    info: &SignatureOnlyConcreteLibfunc,
-) -> Result<()> {
-    Ok(())
-}
-
-/// Select and call the correct libfunc builder function from the selector.
 pub fn build_qm31_is_zero<'ctx, 'this>(
     context: &'ctx Context,
-    registry: &ProgramRegistry<CoreType, CoreLibfunc>,
+    _registry: &ProgramRegistry<CoreType, CoreLibfunc>,
     entry: &'this Block<'ctx>,
     location: Location<'ctx>,
     helper: &LibfuncHelper<'ctx, 'this>,
-    metadata: &mut MetadataStorage,
-    info: &SignatureOnlyConcreteLibfunc,
+    _metadata: &mut MetadataStorage,
+    _info: &SignatureOnlyConcreteLibfunc,
 ) -> Result<()> {
+    let value = entry.arg(0)?;
+    let k0 = entry.const_int(context, location, 0, 144)?;
+
+    let is_zero = entry.cmpi(context, CmpiPredicate::Eq, value, k0, location)?;
+
+    entry.append_operation(helper.cond_br(context, is_zero, [0, 1], [&[], &[value]], location));
+
     Ok(())
 }
 
-/// Select and call the correct libfunc builder function from the selector.
 pub fn build_qm31_bin_operation<'ctx, 'this>(
     context: &'ctx Context,
-    registry: &ProgramRegistry<CoreType, CoreLibfunc>,
+    _registry: &ProgramRegistry<CoreType, CoreLibfunc>,
     entry: &'this Block<'ctx>,
     location: Location<'ctx>,
     helper: &LibfuncHelper<'ctx, 'this>,
-    metadata: &mut MetadataStorage,
+    _metadata: &mut MetadataStorage,
     info: &QM31BinaryOpConcreteLibfunc,
 ) -> Result<()> {
+    let i144_ty = IntegerType::new(context, 144).into();
+
+    let lhs = entry.arg(0)?;
+    let rhs = entry.arg(1)?;
+
+    let result = match info.operator {
+        QM31BinaryOperator::Add => todo!(),
+        QM31BinaryOperator::Sub => {
+            let lhs = entry.extui(lhs, i144_ty, location)?;
+            let rhs = entry.extui(rhs, i144_ty, location)?;
+            let result = entry.append_op_result(arith::subi(lhs, rhs, location))?;
+
+            let qm31_prime =
+                entry.const_int_from_type(context, location, QM31_PRIME.clone(), i144_ty)?;
+            let result_mod = entry.addi(result, qm31_prime, location)?;
+            let is_out_of_range = entry.cmpi(context, CmpiPredicate::Ult, lhs, rhs, location)?;
+
+            entry.append_op_result(arith::select(is_out_of_range, result_mod, result, location))?
+        }
+        QM31BinaryOperator::Mul => todo!(),
+        QM31BinaryOperator::Div => todo!(),
+    };
+
+    entry.append_operation(helper.br(0, &[result], location));
+
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::utils::test::{load_cairo, run_program};
+
+    #[test]
+    fn test_qm31_pack() {
+        let program = load_cairo! {
+            use core::qm31::{QM31Trait, m31, qm31};
+
+            extern fn qm31_const<
+                const W0: m31, const W1: m31, const W2: m31, const W3: m31,
+            >() -> qm31 nopanic;
+
+            fn run_test() {
+                assert!(QM31Trait::new(1, 2, 3, 4) == qm31_const::<1, 2, 3, 4>());
+            }
+        };
+
+        run_program(&program, "run_test", &[]);
+    }
 }
