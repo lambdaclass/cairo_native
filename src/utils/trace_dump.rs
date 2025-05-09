7109dedb30e2b473dfdb6ed5b69f58feb08e8cd7
FrancoGiachetta <francogiachetta27@gmail.com>
2025-05-09 10:33:06 -0300

    remove unnecessary code

--- src/utils/trace_dump.rs
+++ src/utils/trace_dump.rs
@@ -168,46 +168,9 @@ mod tests {
         program
     }
 
-    #[fixture]
-    fn program_circuit() -> Program {
-        let (_, program) = load_cairo!(
-            use core::circuit::{
-                RangeCheck96, AddMod, MulMod, u96, CircuitElement, CircuitInput, circuit_add,
-                circuit_sub, circuit_mul, circuit_inverse, EvalCircuitTrait, u384,
-                CircuitOutputsTrait, CircuitModulus, AddInputResultTrait, CircuitInputs,
-            };
-
-            fn main() -> u384 {
-                let in1 = CircuitElement::<CircuitInput<0>> {};
-                let in2 = CircuitElement::<CircuitInput<1>> {};
-                let add1 = circuit_add(in1, in2);
-                let mul1 = circuit_mul(add1, in1);
-                let mul2 = circuit_mul(mul1, add1);
-                let inv1 = circuit_inverse(mul2);
-                let sub1 = circuit_sub(inv1, in2);
-                let sub2 = circuit_sub(sub1, mul2);
-                let inv2 = circuit_inverse(sub2);
-                let add2 = circuit_add(inv2, inv2);
-
-                let modulus = TryInto::<_, CircuitModulus>::try_into([17, 14, 14, 14]).unwrap();
-
-                let outputs = (add2,)
-                    .new_inputs()
-                    .next([9, 2, 9, 3])
-                    .next([5, 7, 0, 8])
-                    .done()
-                    .eval(modulus)
-                    .unwrap();
-
-                outputs.get_output(add2)
-            }
-        );
-        program
-    }
-
     #[rstest]
-    fn test_program(program_circuit: Program) {
-        let entrypoint_function = &program_circuit
+    fn test_program(program: Program) {
+        let entrypoint_function = &program
             .funcs
             .iter()
             .find(|x| {
@@ -221,7 +184,7 @@ mod tests {
 
         let native_context = NativeContext::new();
         let module = native_context
-            .compile(&program_circuit, false, Some(Default::default()))
+            .compile(&program, false, Some(Default::default()))
             .expect("failed to compile context");
         let executor = AotNativeExecutor::from_native_module(module, OptLevel::default()).unwrap();
 
@@ -230,10 +193,10 @@ mod tests {
             unsafe { *trace_id = 0 };
         }
 
-        TRACE_DUMP.lock().unwrap().insert(
-            0,
-            TraceDump::new(ProgramRegistry::new(&program_circuit).unwrap()),
-        );
+        TRACE_DUMP
+            .lock()
+            .unwrap()
+            .insert(0, TraceDump::new(ProgramRegistry::new(&program).unwrap()));
 
         executor
             .invoke_dynamic(&entrypoint_function.id, &[], Some(u64::MAX))
@@ -248,7 +211,7 @@ mod tests {
             .trace
             .clone();
 
-        let mut vm = VirtualMachine::new(Arc::new(program_circuit));
+        let mut vm = VirtualMachine::new(Arc::new(program));
 
         let initial_gas = u64::MAX;
         let args = [];
