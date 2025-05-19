8668e783902d4005aac9ad98dd830e44d72d4fb1
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-05-19 12:01:58 -0300

    Remove work

--- work/run.fish
+++
@@ -1,4 +0,0 @@
-for file in *.sierra.json
-    set name $(path change-extension '' $(path change-extension '' $file))
-    cargo run --release --bin starknet-native-compile $file "$name.out" -O3
-end
