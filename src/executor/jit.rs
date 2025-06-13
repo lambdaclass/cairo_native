9095d2839c5db9d585508ac0a3d52ea8a81762d8
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-06-13 18:44:05 -0300

    Implement libfunc profiling (squash)
    This commit is a squash of the whole #946 PR
    (https://github.com/lambdaclass/cairo_native/pull/946)

--- src/executor/jit.rs
+++ src/executor/jit.rs
@@ -71,6 +71,9 @@ impl<'m> JitNativeExecutor<'m> {
         #[cfg(feature = "with-trace-dump")]
         crate::metadata::trace_dump::setup_runtime(|name| executor.find_symbol_ptr(name));
 
+        #[cfg(feature = "with-libfunc-profiling")]
+        crate::metadata::profiler::setup_runtime(|name| executor.find_symbol_ptr(name));
+
         Ok(executor)
     }
 
