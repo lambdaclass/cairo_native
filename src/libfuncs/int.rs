f98e6f5e09321544e56956645ecc7dc94686dac4
DiegoC <90105443+DiegoCivi@users.noreply.github.com>
2025-07-15 12:48:56 -0300

    Fix build_square_root() range_check increment (#1298)
    * Increment range_check by 4
    
    * Add comment
    
    ---------
    
    Co-authored-by: Gabriel Bosio <38794644+gabrielbosio@users.noreply.github.com>

--- src/libfuncs/int.rs
+++ src/libfuncs/int.rs
@@ -630,7 +630,10 @@ fn build_square_root<'ctx, 'this>(
     _metadata: &mut MetadataStorage,
     info: &SignatureOnlyConcreteLibfunc,
 ) -> Result<()> {
-    let range_check = super::increment_builtin_counter(context, entry, location, entry.arg(0)?)?;
+    // The sierra-to-casm compiler uses the range_check builtin 4 times.
+    // https://github.com/starkware-libs/cairo/blob/96625b57abee8aca55bdeb3ecf29f82e8cea77c3/crates/cairo-lang-sierra-to-casm/src/invocations/int/unsigned.rs#L73
+    let range_check =
+        super::increment_builtin_counter_by(context, entry, location, entry.arg(0)?, 4)?;
 
     let input = entry.arg(1)?;
     let (input_bits, value_bits) =
