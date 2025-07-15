6b784f0ddab37d800198a4e675bbf52366337fa9
Diego Civini <diego.civini@lambdaclass.com>
2025-07-15 18:19:27 -0300

    Change url

--- src/libfuncs/ec.rs
+++ src/libfuncs/ec.rs
@@ -179,7 +179,7 @@ pub fn build_point_from_x<'ctx, 'this>(
 
     // The sierra-to-casm compiler uses the range check builtin a total of 3 times if the
     // point is on the curve. Otherwise it is not used.
-    // https://github.com/starkware-libs/cairo/blob/ff2e19a3d671036e626ea89cac409c5325383584/crates/cairo-lang-sierra-to-casm/src/invocations/ec.rs#L167
+    // https://github.com/starkware-libs/cairo/blob/v2.12.0-dev.1/crates/cairo-lang-sierra-to-casm/src/invocations/ec.rs#L167
     let range_check = increment_builtin_counter_by_if(
         context,
         entry,
