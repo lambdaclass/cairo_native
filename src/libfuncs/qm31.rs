825225e2851ac783dd2b4a3104895ad0807e2fbb
Diego Civini <diego.civini@lambdaclass.com>
2025-10-18 20:17:15 -0300

    Begin with libfuncs

---
+++ src/libfuncs/qm31.rs
@@ -0,0 +1,125 @@
+use crate::error::Result;
+use cairo_lang_sierra::{
+    extensions::{
+        core::{CoreLibfunc, CoreType},
+        lib_func::SignatureOnlyConcreteLibfunc,
+        qm31::{QM31BinaryOpConcreteLibfunc, QM31Concrete, QM31ConstConcreteLibfunc},
+    },
+    program_registry::ProgramRegistry,
+};
+use melior::{
+    dialect::arith::CmpiPredicate,
+    helpers::{ArithBlockExt, BuiltinBlockExt},
+    ir::{Block, Location, Value, ValueLike},
+    Context,
+};
+
+use crate::{libfuncs::LibfuncHelper, metadata::MetadataStorage};
+
+pub fn build<'ctx, 'this>(
+    context: &'ctx Context,
+    registry: &ProgramRegistry<CoreType, CoreLibfunc>,
+    entry: &'this Block<'ctx>,
+    location: Location<'ctx>,
+    helper: &LibfuncHelper<'ctx, 'this>,
+    metadata: &mut MetadataStorage,
+    selector: &QM31Concrete,
+) -> Result<()> {
+    match selector {
+        QM31Concrete::BinaryOperation(info) => {
+            build_binary_op(context, registry, entry, location, helper, metadata, info)
+        }
+        QM31Concrete::Const(info) => {
+            build_const(context, registry, entry, location, helper, metadata, info)
+        }
+        QM31Concrete::IsZero(info) => {
+            build_is_zero(context, registry, entry, location, helper, metadata, info)
+        }
+        QM31Concrete::Pack(info) => {
+            build_pack(context, registry, entry, location, helper, metadata, info)
+        }
+        QM31Concrete::Unpack(info) => {
+            build_unpack(context, registry, entry, location, helper, metadata, info)
+        }
+        QM31Concrete::FromM31(info) => {
+            build_from_m31(context, registry, entry, location, helper, metadata, info)
+        }
+    }
+}
+
+pub fn build_const<'ctx, 'this>(
+    context: &'ctx Context,
+    _registry: &ProgramRegistry<CoreType, CoreLibfunc>,
+    entry: &'this Block<'ctx>,
+    location: Location<'ctx>,
+    helper: &LibfuncHelper<'ctx, 'this>,
+    _metadata: &mut MetadataStorage,
+    _info: &QM31ConstConcreteLibfunc,
+) -> Result<()> {
+    todo!()
+}
+
+pub fn build_is_zero<'ctx, 'this>(
+    context: &'ctx Context,
+    _registry: &ProgramRegistry<CoreType, CoreLibfunc>,
+    entry: &'this Block<'ctx>,
+    location: Location<'ctx>,
+    helper: &LibfuncHelper<'ctx, 'this>,
+    _metadata: &mut MetadataStorage,
+    _info: &SignatureOnlyConcreteLibfunc,
+) -> Result<()> {
+    let qm31: Value = entry.arg(0)?;
+
+    let k0 = entry.const_int_from_type(context, location, 0, qm31.r#type())?;
+    let is_zero = entry.cmpi(context, CmpiPredicate::Eq, qm31, k0, location)?;
+
+    helper.cond_br(context, entry, is_zero, [0, 1], [&[], &[qm31]], location)
+}
+
+pub fn build_binary_op<'ctx, 'this>(
+    context: &'ctx Context,
+    _registry: &ProgramRegistry<CoreType, CoreLibfunc>,
+    entry: &'this Block<'ctx>,
+    location: Location<'ctx>,
+    helper: &LibfuncHelper<'ctx, 'this>,
+    _metadata: &mut MetadataStorage,
+    _info: &QM31BinaryOpConcreteLibfunc,
+) -> Result<()> {
+    todo!()
+}
+
+pub fn build_pack<'ctx, 'this>(
+    context: &'ctx Context,
+    _registry: &ProgramRegistry<CoreType, CoreLibfunc>,
+    entry: &'this Block<'ctx>,
+    location: Location<'ctx>,
+    helper: &LibfuncHelper<'ctx, 'this>,
+    _metadata: &mut MetadataStorage,
+    _info: &SignatureOnlyConcreteLibfunc,
+) -> Result<()> {
+    todo!()
+}
+
+pub fn build_unpack<'ctx, 'this>(
+    context: &'ctx Context,
+    _registry: &ProgramRegistry<CoreType, CoreLibfunc>,
+    entry: &'this Block<'ctx>,
+    location: Location<'ctx>,
+    helper: &LibfuncHelper<'ctx, 'this>,
+    _metadata: &mut MetadataStorage,
+    _info: &SignatureOnlyConcreteLibfunc,
+) -> Result<()> {
+    todo!()
+}
+
+pub fn build_from_m31<'ctx, 'this>(
+    context: &'ctx Context,
+    _registry: &ProgramRegistry<CoreType, CoreLibfunc>,
+    entry: &'this Block<'ctx>,
+    location: Location<'ctx>,
+    helper: &LibfuncHelper<'ctx, 'this>,
+    _metadata: &mut MetadataStorage,
+    _info: &SignatureOnlyConcreteLibfunc,
+) -> Result<()> {
+    todo!()
+}
