470c2c30215054a72ca32518cac9d4241b7950d1
DiegoC <90105443+DiegoCivi@users.noreply.github.com>
2025-10-30 10:11:37 -0300

    Update src/libfuncs/qm31.rs
    Co-authored-by: Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>

--- src/libfuncs/qm31.rs
+++ src/libfuncs/qm31.rs
@@ -274,7 +274,7 @@ pub fn build_unpack<'ctx, 'this>(
 ///
 /// # Constraints
 ///
-/// m31 are always between 0 and 2**31 - 2
+/// m31 are always between 0 and 2**31 - 2 (inclusive)
 ///
 /// # Cairo Signature
 ///
