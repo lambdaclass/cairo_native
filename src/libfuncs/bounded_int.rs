1ad0def94324f0c15fa1307a275210889e98f9cc
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-11-26 13:08:15 -0300

    Add more test cases

--- src/libfuncs/bounded_int.rs
+++ src/libfuncs/bounded_int.rs
@@ -1370,26 +1370,48 @@ mod test {
             #[feature("bounded-int-utils")]
             use core::internal::bounded_int::{self, BoundedInt, div_rem, DivRemHelper};
             use core::internal::OptionRev;
+            extern fn bounded_int_wrap_non_zero<T>(v: T) -> NonZero<T> nopanic;
 
 
             impl Helper_u8_u8 of DivRemHelper<u8, u8> {
                 type DivT = u8;
                 type RemT = u8;
             }
-
             fn test_u8(a: felt252, b: felt252) -> (felt252, felt252) {
                 let a_int: u8 = a.try_into().unwrap();
                 let b_int: u8 = b.try_into().unwrap();
                 let b_nz: NonZero<u8> = b_int.try_into().unwrap();
-                let (q, r) = div_rem::<u8, u8, Helper_u8_u8>(a_int, b_nz);
+                let (q, r) = div_rem(a_int, b_nz);
+                return (q.into(), r.into());
+            }
+
+            impl Helper_10_100_10_40 of DivRemHelper<BoundedInt<10, 100>, BoundedInt<10, 40>> {
+                type DivT = BoundedInt<0, 10>;
+                type RemT = BoundedInt<0, 39>;
+            }
+            fn test_10_100_10_40(a: felt252, b: felt252) -> (felt252, felt252) {
+                let a_int: BoundedInt<10, 100> = a.try_into().unwrap();
+                let b_int: BoundedInt<10, 40> = b.try_into().unwrap();
+                let (q, r) = div_rem(a_int, bounded_int_wrap_non_zero(b_int));
+                return (q.into(), r.into());
+            }
+
+            impl Helper_50_100_20_40 of DivRemHelper<BoundedInt<50, 100>, BoundedInt<20, 40>> {
+                type DivT = BoundedInt<1, 5>;
+                type RemT = BoundedInt<0, 39>;
+            }
+            fn test_50_100_20_40(a: felt252, b: felt252) -> (felt252, felt252) {
+                let a_int: BoundedInt<50, 100> = a.try_into().unwrap();
+                let b_int: BoundedInt<20, 40> = b.try_into().unwrap();
+                let (q, r) = div_rem(a_int, bounded_int_wrap_non_zero(b_int));
                 return (q.into(), r.into());
             }
         };
     }
 
-    #[test_case("test_u8", 0, 1, 0, 0)]
-    #[test_case("test_u8", 100, 3, 33, 1)]
-    #[test_case("test_u8", 255, 50, 5, 5)]
+    #[test_case("test_u8", 100, 30, 3, 10)]
+    #[test_case("test_10_100_10_40", 100, 30, 3, 10)]
+    #[test_case("test_50_100_20_40", 100, 30, 3, 10)]
     fn test_div_rem(entry_point: &str, a: i32, b: i32, expected_q: u32, expected_r: u32) {
         let arguments = &[Felt252::from(a).into(), Felt252::from(b).into()];
         let execution = run_program(&TEST_DIV_REM_PROGRAM, entry_point, arguments);
