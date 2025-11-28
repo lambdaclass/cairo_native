996bdc552b6493177ba73d984db0a19497f5f7cc
Diego Civini <diego.civini@lambdaclass.com>
2025-11-28 16:21:07 -0300

    Refactor of tests names

--- src/libfuncs/bounded_int.rs
+++ src/libfuncs/bounded_int.rs
@@ -882,88 +882,88 @@ mod test {
                         #[feature("bounded-int-utils")]
             use core::internal::bounded_int::{self, BoundedInt, MulHelper, mul, UnitInt};
 
-            impl MulHelper1 of MulHelper<BoundedInt<-128, 127>, BoundedInt<-128, 127>> {
+            impl MulHelperBI_m128x127_BI_m128x127 of MulHelper<BoundedInt<-128, 127>, BoundedInt<-128, 127>> {
                 type Result = BoundedInt<-16256, 16384>;
             }
 
-            impl MulHelper2 of MulHelper<BoundedInt<0, 128>, BoundedInt<0, 128>> {
+            impl MulHelperBI_0x128_BI_0x128 of MulHelper<BoundedInt<0, 128>, BoundedInt<0, 128>> {
                 type Result = BoundedInt<0, 16384>;
             }
 
-            impl MulHelper3 of MulHelper<BoundedInt<1, 31>, BoundedInt<1, 1>> {
+            impl MulHelperBI_1x31_BI_1x1 of MulHelper<BoundedInt<1, 31>, BoundedInt<1, 1>> {
                 type Result = BoundedInt<1, 31>;
             }
 
-            impl MulHelper4 of MulHelper<BoundedInt<-1, 31>, BoundedInt<-1, -1>> {
+            impl MulHelperBI_m1x31_BI_m1xm1 of MulHelper<BoundedInt<-1, 31>, BoundedInt<-1, -1>> {
                 type Result = BoundedInt<-31, 1>;
             }
 
-            impl MulHelper5 of MulHelper<BoundedInt<31, 31>, BoundedInt<1, 1>> {
+            impl MulHelperBI_31x31_BI_1x1 of MulHelper<BoundedInt<31, 31>, BoundedInt<1, 1>> {
                 type Result = BoundedInt<31, 31>;
             }
 
-            impl MulHelper6 of MulHelper<BoundedInt<-100, 0>, BoundedInt<0, 100>> {
+            impl MulHelperBI_m10x0_BI_0x100 of MulHelper<BoundedInt<-100, 0>, BoundedInt<0, 100>> {
                 type Result = BoundedInt<-10000, 0>;
             }
 
-            impl MulHelper7 of MulHelper<BoundedInt<1, 1>, BoundedInt<1, 1>> {
+            impl MulHelperBI_1x1_BI_1x1 of MulHelper<BoundedInt<1, 1>, BoundedInt<1, 1>> {
                 type Result = BoundedInt<1, 1>;
             }
 
-            impl MulHelper8 of MulHelper<BoundedInt<-5, 5>, UnitInt<2>> {
+            impl MulHelperBI_m5x5_UI_2 of MulHelper<BoundedInt<-5, 5>, UnitInt<2>> {
                 type Result = BoundedInt<-10, 10>;
             }
 
-            fn run_test_1(a: felt252, b: felt252) -> BoundedInt<-16256, 16384> {
+            fn bi_m128x127_times_bi_m128x127(a: felt252, b: felt252) -> BoundedInt<-16256, 16384> {
                 let a: BoundedInt<-128, 127> = a.try_into().unwrap();
                 let b: BoundedInt<-128, 127> = b.try_into().unwrap();
 
                 mul(a,b)
             }
 
-            fn run_test_2(a: felt252, b: felt252) -> BoundedInt<0, 16384> {
+            fn bi_0x128_times_bi_0x128(a: felt252, b: felt252) -> BoundedInt<0, 16384> {
                 let a: BoundedInt<0, 128> = a.try_into().unwrap();
                 let b: BoundedInt<0, 128> = b.try_into().unwrap();
 
                 mul(a,b)
             }
 
-            fn run_test_3(a: felt252, b: felt252) -> BoundedInt<1, 31> {
+            fn bi_1x31_times_bi_1x1(a: felt252, b: felt252) -> BoundedInt<1, 31> {
                 let a: BoundedInt<1, 31> = a.try_into().unwrap();
                 let b: BoundedInt<1, 1> = b.try_into().unwrap();
 
                 mul(a,b)
             }
 
-            fn run_test_4(a: felt252, b: felt252) -> BoundedInt<-31, 1> {
+            fn bi_m1x31_times_bi_m1xm1(a: felt252, b: felt252) -> BoundedInt<-31, 1> {
                 let a: BoundedInt<-1, 31> = a.try_into().unwrap();
                 let b: BoundedInt<-1, -1> = b.try_into().unwrap();
 
                 mul(a,b)
             }
 
-            fn run_test_5(a: felt252, b: felt252) -> BoundedInt<31, 31> {
+            fn bi_31x31_times_bi_1x1(a: felt252, b: felt252) -> BoundedInt<31, 31> {
                 let a: BoundedInt<31, 31> = a.try_into().unwrap();
                 let b: BoundedInt<1, 1> = b.try_into().unwrap();
 
                 mul(a,b)
             }
 
-            fn run_test_6(a: felt252, b: felt252) -> BoundedInt<-10000, 0> {
+            fn bi_m100x0_times_bi_0x100(a: felt252, b: felt252) -> BoundedInt<-10000, 0> {
                 let a: BoundedInt<-100, 0> = a.try_into().unwrap();
                 let b: BoundedInt<0, 100> = b.try_into().unwrap();
 
                 mul(a,b)
             }
 
-            fn run_test_7(a: felt252, b: felt252) -> BoundedInt<1, 1> {
+            fn bi_1x1_times_bi_1x1(a: felt252, b: felt252) -> BoundedInt<1, 1> {
                 let a: BoundedInt<1, 1> = a.try_into().unwrap();
                 let b: BoundedInt<1, 1> = b.try_into().unwrap();
 
                 mul(a,b)
             }
 
-            fn run_test_8(a: felt252, b: felt252) -> BoundedInt<-10, 10> {
+            fn bi_m5x5_times_ui_2(a: felt252, b: felt252) -> BoundedInt<-10, 10> {
                 let a: BoundedInt<-5, 5> = a.try_into().unwrap();
                 let b: UnitInt<2> = b.try_into().unwrap();
 
@@ -972,14 +972,14 @@ mod test {
         };
     }
 
-    #[test_case("run_test_1", -128, -128, 16384)]
-    #[test_case("run_test_2", 126, 128, 16128)]
-    #[test_case("run_test_3", 31, 1, 31)]
-    #[test_case("run_test_4", 31, -1, -31)]
-    #[test_case("run_test_5", 31, 1, 31)]
-    #[test_case("run_test_6", -100, 100, -10000)]
-    #[test_case("run_test_7", 1, 1, 1)]
-    #[test_case("run_test_8", -3, 2, -6)]
+    #[test_case("bi_m128x127_times_bi_m128x127", -128, -128, 16384)]
+    #[test_case("bi_0x128_times_bi_0x128", 126, 128, 16128)]
+    #[test_case("bi_1x31_times_bi_1x1", 31, 1, 31)]
+    #[test_case("bi_m1x31_times_bi_m1xm1", 31, -1, -31)]
+    #[test_case("bi_31x31_times_bi_1x1", 31, 1, 31)]
+    #[test_case("bi_m100x0_times_bi_0x100", -100, 100, -10000)]
+    #[test_case("bi_1x1_times_bi_1x1", 1, 1, 1)]
+    #[test_case("bi_m5x5_times_ui_2", -3, 2, -6)]
     fn test_mul(entry_point: &str, lhs: i32, rhs: i32, expected_result: i32) {
         let result = run_program(
             &TEST_MUL_PROGRAM,
