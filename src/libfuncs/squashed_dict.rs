bfab24c6c00f0d7500beced6c90443ba7dc5ed81
Diego Civini <diego.civini@lambdaclass.com>
2025-12-02 14:59:38 -0300

    Begin for structure to append tuples

--- src/libfuncs/squashed_dict.rs
+++ src/libfuncs/squashed_dict.rs
@@ -18,9 +18,12 @@ use cairo_lang_sierra::{
     program_registry::ProgramRegistry,
 };
 use melior::{
-    dialect::llvm,
+    dialect::{llvm, scf},
     helpers::{ArithBlockExt, BuiltinBlockExt, GepIndex, LlvmBlockExt},
-    ir::{r#type::IntegerType, Block, Location, Value},
+    ir::{
+        operation::OperationBuilder, r#type::IntegerType, Block, BlockLike, Location, OperationRef,
+        Region, Type, Value, ValueLike,
+    },
     Context,
 };
 use std::alloc::Layout;
@@ -57,6 +60,152 @@ fn get_inner_type_layout<'ctx, 'this>(
     let (_, elem_layout) = registry.build_type_with_layout(context, helper, metadata, &info.ty)?;
 
     Ok(elem_layout)
+}
+
+/// Get the inner types of the tuple
+fn get_inner_types<'ctx, 'this>(
+    context: &'ctx Context,
+    registry: &ProgramRegistry<CoreType, CoreLibfunc>,
+    helper: &LibfuncHelper<'ctx, 'this>,
+    metadata: &mut MetadataStorage,
+    info: &SignatureAndTypeConcreteLibfunc,
+) -> Result<(Type<'ctx>, Type<'ctx>)> {
+    let array_ty = registry.get_type(&info.signature.branch_signatures[0].vars[0].ty)?;
+    let CoreTypeConcrete::Array(info) = array_ty else {
+        native_panic!("Expected Array");
+    };
+    let struct_ty = registry.get_type(&info.ty)?;
+    let CoreTypeConcrete::Struct(info) = struct_ty else {
+        native_panic!("Expected Struct");
+    };
+    let felt_ty_id = &info.members[0];
+    let generic_ty_id = &info.members[1];
+
+    let felt_ty = registry.build_type(context, helper, metadata, felt_ty_id)?;
+    let generic_ty = registry.build_type(context, helper, metadata, generic_ty_id)?;
+
+    Ok((felt_ty, generic_ty))
+}
+
+fn append_entry_tuples<'ctx, 'this>(
+    context: &'ctx Context,
+    registry: &ProgramRegistry<CoreType, CoreLibfunc>,
+    entry: &'this Block<'ctx>,
+    location: Location<'ctx>,
+    helper: &LibfuncHelper<'ctx, 'this>,
+    metadata: &mut MetadataStorage,
+    info: &SignatureAndTypeConcreteLibfunc,
+    arr: Value<'ctx, 'this>,
+    dict_len: Value<'ctx, 'this>,
+    elem_stride: Value<'ctx, 'this>,
+) -> Result<Value<'ctx, 'this>> {
+    let region = {
+        let region = Region::new();
+        let block = region.append_block(Block::new(&[
+            (IntegerType::new(context, 64).into(), location),
+            (arr.r#type(), location),
+        ]));
+        let loop_arr = block.arg(1)?;
+        let end_offset = block.extract_value(
+            context,
+            location,
+            loop_arr,
+            IntegerType::new(context, 32).into(),
+            2,
+        )?;
+
+        metadata
+            .get_mut::<DebugUtils>()
+            .unwrap()
+            .print_i32(context, helper, &block, end_offset, location)?;
+
+        let k1 = block.const_int(context, location, 1, 32)?;
+        let end_offset = block.addi(end_offset, k1, location)?;
+
+        let new_arr = block.insert_value(context, location, loop_arr, end_offset, 2)?;
+        block.append_operation(scf::r#yield(&[new_arr], location));
+        region
+    };
+
+    let k0 = entry.const_int(context, location, 0, 64)?;
+    let k1 = entry.const_int(context, location, 1, 64)?;
+
+    Ok(entry.append_op_result(
+        OperationBuilder::new("scf.for", location)
+            .add_operands(&[k0, dict_len, k1])
+            .add_operands(&[arr])
+            .add_results(&[arr.r#type()])
+            .add_regions([region])
+            .build()?,
+    )?)
+
+    // Ok(entry.append_operation(scf::r#for(
+    //     k0,
+    //     dict_len,
+    //     k1,
+    //     {
+    //         let region = Region::new();
+    //         let block = region.append_block(Block::new(&[(
+    //             IntegerType::new(context, 64).into(),
+    //             location,
+    //         )])); // TODO: Do I need to pass the arr as an argument?
+
+    //         metadata.get_mut::<DebugUtils>().unwrap().debug_print(
+    //             context,
+    //             helper,
+    //             &block,
+    //             "LOOP ITERATION",
+    //             location,
+    //         )?;
+
+    //         let array_ptr = block.extract_value(
+    //             context,
+    //             location,
+    //             arr,
+    //             llvm::r#type::pointer(context, 0),
+    //             0,
+    //         )?;
+    //         let end_offset = block.extract_value(
+    //             context,
+    //             location,
+    //             arr,
+    //             IntegerType::new(context, 32).into(),
+    //             2,
+    //         )?;
+    //         let end_offset =
+    //             block.extui(end_offset, IntegerType::new(context, 64).into(), location)?;
+    //         let target_offset = block.muli(end_offset, elem_stride, location)?;
+    //         let target_ptr = block.gep(
+    //             context,
+    //             location,
+    //             array_ptr,
+    //             &[GepIndex::Value(target_offset)],
+    //             IntegerType::new(context, 8).into(),
+    //         )?;
+
+    //         // Build tuple and store it
+    //         let (felt_ty, generic_ty) = get_inner_types(context, registry, helper, metadata, info)?;
+    //         let tuple = block.append_op_result(llvm::undef(
+    //             llvm::r#type::r#struct(context, &[felt_ty, generic_ty, generic_ty], false),
+    //             location,
+    //         ))?;
+    //         block.store(context, location, target_ptr, tuple)?;
+
+    //         // Update end offset
+    //         let end_offset = block.addi(end_offset, k1, location)?;
+    //         let end_offset =
+    //             entry.trunci(end_offset, IntegerType::new(context, 32).into(), location)?;
+    //         metadata
+    //             .get_mut::<DebugUtils>()
+    //             .unwrap()
+    //             .print_i32(context, helper, &block, end_offset, location)?;
+    //         block.insert_value(context, location, arr, end_offset, 2)?;
+
+    //         block.append_operation(scf::r#yield(&[], location)); // TODO: Do I need to return the arr here?
+    //         region
+    //     },
+    //     location,
+    // )))
 }
 
 fn build_entries_array<'ctx, 'this>(
@@ -98,7 +247,7 @@ fn build_entries_array<'ctx, 'this>(
     ))?;
     let allocated_capacity =
         entry.trunci(dict_len, IntegerType::new(context, 32).into(), location)?; // TODO: Check if this is the correct capacity
-    let end_offset = entry.trunci(dict_len, IntegerType::new(context, 32).into(), location)?; // TODO: Check if this is the correct end_offset. Is it the number of bytes or the quantity of elements
+    let end_offset = entry.const_int_from_type(context, location, 0, len_ty)?; // TODO: Check if this is the correct end_offset. Is it the number of bytes or the quantity of elements
     let start_offset = entry.const_int_from_type(context, location, 0, len_ty)?; // TODO: What value should go here? I think it is okay to have 0.
 
     let arr = entry.insert_values(
@@ -152,6 +301,20 @@ fn build_entries_array<'ctx, 'this>(
     // Insert the pointer to the data pointer inside the array
     let arr = entry.insert_value(context, location, arr, array_ptr_ptr, 0)?;
 
+    // Append a tuple for each entry in the dictionary
+    let arr = append_entry_tuples(
+        context,
+        registry,
+        entry,
+        location,
+        helper,
+        metadata,
+        info,
+        arr,
+        dict_len,
+        elem_stride,
+    )?;
+
     Ok(arr)
 }
 
@@ -169,10 +332,10 @@ pub fn build_into_entries<'ctx, 'this>(
         build_entries_array(context, registry, entry, location, helper, metadata, info)?;
 
     // Get the ptr to the data and pass it to the runtime function
-    let ptr_ty = llvm::r#type::pointer(context, 0);
-    let data_ptr_ptr = entry.extract_value(context, location, entries_array, ptr_ty, 0)?;
-    let data_ptr = entry.load(context, location, data_ptr_ptr, ptr_ty)?;
-    let dict_ptr = entry.arg(0)?;
+    // let ptr_ty = llvm::r#type::pointer(context, 0);
+    // let data_ptr_ptr = entry.extract_value(context, location, entries_array, ptr_ty, 0)?;
+    // let data_ptr = entry.load(context, location, data_ptr_ptr, ptr_ty)?;
+    // let dict_ptr = entry.arg(0)?;
 
     // Call runtime function that pushes the tuples into the array
     // metadata
@@ -180,22 +343,18 @@ pub fn build_into_entries<'ctx, 'this>(
     //     .ok_or(Error::MissingMetadata)?
     //     .dict_into_entries(context, helper, entry, dict_ptr, data_ptr, location)?;
 
-    // let len_ty = IntegerType::new(context, 32).into();
+    let len_ty = IntegerType::new(context, 32).into();
     // let start_off = entry.extract_value(context, location, entries_array, len_ty, 1)?;
-    // let end_off = entry.extract_value(context, location, entries_array, len_ty, 2)?;
+    let end_off = entry.extract_value(context, location, entries_array, len_ty, 2)?;
     // let capacity = entry.extract_value(context, location, entries_array, len_ty, 3)?;
-    metadata
-        .get_mut::<DebugUtils>()
-        .unwrap()
-        .print_pointer(context, helper, entry, data_ptr, location)?;
     // metadata
     //     .get_mut::<DebugUtils>()
     //     .unwrap()
     //     .print_i32(context, helper, entry, start_off, location)?;
-    // metadata
-    //     .get_mut::<DebugUtils>()
-    //     .unwrap()
-    //     .print_i32(context, helper, entry, end_off, location)?;
+    metadata
+        .get_mut::<DebugUtils>()
+        .unwrap()
+        .print_i32(context, helper, entry, end_off, location)?;
     // metadata
     //     .get_mut::<DebugUtils>()
     //     .unwrap()
