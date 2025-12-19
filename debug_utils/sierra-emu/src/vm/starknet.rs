2ef63742f52ceaaa6966fa8ccf1860ede7619097
FrancoGiachetta <francogiachetta27@gmail.com>
2025-12-19 10:52:37 -0300

    fix secp256_get_point_from_x and secp256_new implementation

--- debug_utils/sierra-emu/src/vm/starknet.rs
+++ debug_utils/sierra-emu/src/vm/starknet.rs
@@ -174,7 +174,7 @@ fn eval_secp256_new(
                 None => Value::Enum {
                     self_ty: enum_ty.clone(),
                     index: 1,
-                    payload: Box::new(Value::Unit),
+                    payload: Box::new(Value::Struct(vec![])),
                 },
             };
 
@@ -328,7 +328,7 @@ fn eval_secp256_get_point_from_x(
                 None => Value::Enum {
                     self_ty: enum_ty.clone(),
                     index: 1,
-                    payload: Box::new(Value::Unit),
+                    payload: Box::new(Value::Struct(vec![])),
                 },
             };
 
