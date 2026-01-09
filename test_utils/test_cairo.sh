e7a6df4595b55bca0e22819bee6503a773f23bc5
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2026-01-09 16:07:20 -0300

    Remove globstar

--- test_utils/test_cairo.sh
+++ test_utils/test_cairo.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 set -eu
-shopt -s globstar
 
 cargo build --package cairo-native-test-2
 echo
 
-for test_file in test_data_artifacts/**/*.tests.json; do
+find test_data_artifacts -name "*.tests.json" -print0 |
+while IFS= read -r -d '' test_file; do
 	echo "testing $test_file..."
 	echo
 	target/debug/cairo-native-test-2 $test_file -O2 --compare-with-cairo-vm
