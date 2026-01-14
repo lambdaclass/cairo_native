b1b20805accf1f4c7b3ff5d9f7262d93a35bc16f
Diego Civini <diego.civini@lambdaclass.com>
2026-01-14 18:22:24 -0300

    Fix path for alexandria tests

--- tests/tests/alexandria.rs
+++ tests/tests/alexandria.rs
@@ -8,7 +8,8 @@ use test_case::test_case;
 
 #[track_caller]
 fn compare_inputless_function(function_name: &str) {
-    let program: (String, Program, SierraCasmRunner) = get_compiled_program("test_data_artifacts/programs/alexandria");
+    let program: (String, Program, SierraCasmRunner) =
+        get_compiled_program("test_data_artifacts/scarb/alexandria/alexandria");
     let result_vm =
         run_vm_program(&program, function_name, vec![], Some(DEFAULT_GAS as usize)).unwrap();
     let result_native = run_native_program(
